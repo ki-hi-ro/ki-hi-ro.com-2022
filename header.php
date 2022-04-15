0ff5e71f392e069b26e4c3d81c8c7a2a50f27f07
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-15 19:20:05 +0900

    キャッチコピーの追加

--- header.php
+++ header.php
@@ -47,18 +47,19 @@
 
 	<header itemscope="itemscope" itemtype="http://schema.org/WPHeader">
 		<div class="container header-container">
-			<div class="header-nav row align-items-center">
+			<div class="header-nav row">
 
 				<div class="blogname">
-					<?php if (is_front_page()) { ?>
-					<h1 class="title">
-					<?php } ?>
+					<?php if (is_front_page()) : ?>
+						<h1 class="title">
+					<?php endif; ?>
 						<a href="<?php echo home_url(); ?>" class="sitename">
-								<span class="sitename main">ki-hi-ro.com</span>
+							<span class="sitename main">ki-hi-ro.com</span>
 						</a>
-					<?php if (is_front_page()) { ?>
-					</h1>
-					<?php } ?>
+					<?php if (is_front_page()) : ?>
+						</h1>
+					<?php endif; ?>
+					<span class="sitename sub">WEB制作フリーランスエンジニア</span>
 				</div>
 
 				<nav class="nav-menu">
