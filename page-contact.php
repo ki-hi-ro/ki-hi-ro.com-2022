b7ca7c8268fcc85c1d8121dd1b0d32c6300ff2d1
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-17 16:21:46 +0900

    細かい修正

--- page-contact.php
+++ page-contact.php
@@ -2,12 +2,12 @@
 
 <div class="author col-xs-12 calendar single-wrap" itemprop="author" itemscope itemtype="http://schema.org/Person">
   <h1><?php the_title(); ?></h1>
-	<div class="author__contents">  
+	<div class="author__contents">
     <p>私は普段、HTML / CSS やWordPress、EC-CUBEなどを使用したコーディングをメインで行っています。</p>
     <p>WEBサイトの更新などでフリーランスのエンジニアをお探しの方は、ぜひお気軽に、<br>下記フォームからご連絡ください。</p>
     <iframe class="pc-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="868" height="830" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
     <iframe class="tb-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="690" height="830" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-    <iframe class="sp-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+    <iframe class="sp-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
   </div>
 </div>
 
--- single.php
+++ single.php
@@ -15,12 +15,6 @@
               <p class="last-modified-date-time">最終更新日時は、<?php the_modified_date("Y年n月j日 ${today_week}曜日 G時${minute}分"); ?>です。</p>
             <?php endif; ?>
             <?php the_content(); ?>
-            <h2>お問い合わせ</h2>
-            <p>私は普段、HTML / CSS やWordPress、EC-CUBEなどを使用したコーディングをメインで行っています。</p>
-          <p>WEBサイトの更新などでフリーランスのエンジニアをお探しの方は、ぜひお気軽に、<br>下記フォームからご連絡ください。</p>
-          <iframe class="pc-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="868" height="830" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-          <iframe class="tb-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="690" height="830" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-          <iframe class="sp-form" src="https://docs.google.com/forms/d/e/1FAIpQLSc5ir6fbnFHcuVu0EZtuRBCGEVOp7hxcUlKKZyxfW-UsMF2jw/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
         <?php endwhile; ?>
     <?php endif; ?>
 	</div>
