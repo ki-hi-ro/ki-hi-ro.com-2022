093d1905c8e04c5570d664f0e30037e46beeee46
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-19 17:21:44 +0900

    サービスお問い合わせページの調整

--- page-service-contact.php
+++ page-service-contact.php
@@ -3,7 +3,7 @@
 <div class="top-box col-xs-12">
   <figure class="page-mv">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service/service-mv.png" alt="サービス メイン画像">
-		<h1>サービス<br>お申し込み</h1>
+		<h1>サービス<br>お問い合わせ</h1>
   </figure>
 
   <div class="page__contents-container">
