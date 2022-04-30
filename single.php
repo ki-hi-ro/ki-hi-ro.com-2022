6407c4dd9a0fbc32f688f2bab4e03773d95d91e1
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-30 13:48:31 +0900

    関連記事が出力されるようにした

--- single.php
+++ single.php
@@ -17,6 +17,29 @@
             <?php the_content(); ?>
         <?php endwhile; ?>
     <?php endif; ?>
+    <h4>関連記事</h4>
+    <?php
+        $my_query = new WP_Query();
+        global $post;
+        $post_id = $post->ID;
+        $taxonomy = 'post_tag';
+        $term = get_the_terms($post_id, $taxonomy);
+        $term_slug =  $term[0]->slug;
+
+        $args = array(
+          'post_type' => 'post',
+          'tag'    => $term_slug,
+          'post__not_in'   => array( $post->ID ),
+          'posts_per_page' => 6,
+          'orderby' => 'date',
+          'order' => 'DESC',
+        );
+        $my_query->query( $args ); //データ取得
+    ?>
+    <?php if( $my_query->have_posts() ): while( $my_query->have_posts() ) : $my_query->the_post();?>
+        <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+
 	</div>
 </div>
 
