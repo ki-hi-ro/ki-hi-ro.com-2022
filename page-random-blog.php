e01ef7154dc3c031480a33a26a398b374ad5f8fd
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-06-04 12:48:10 +0900

    ランダムに3記事出力させた

--- page-random-blog.php
+++ page-random-blog.php
@@ -2,6 +2,39 @@
 
 <?php $slug = 'random-blog'; ?>
 
+<h2>雑記ブログ</h2>
+<section class="top-new-blog">
+	<?php
+		$cat_posts = get_posts(array(
+			'post_type' => 'post',
+			'category_name' => $slug,
+			'posts_per_page' => 3,
+			'orderby' => 'rand'
+		));
+		global $post;
+		if ($cat_posts) : foreach ($cat_posts as $post) : setup_postdata($post); ?>
+				<a href="<?php the_permalink() ?>" class="new-blog__post top-card">
+					<p class="post__date"><?php the_time('Y/m/d') ?></p>
+					<h3><?php the_title(); ?></h3>
+					<span class="post__category">
+						<?php $cat = get_the_category();
+							$cat = $cat[0]; {
+								echo $cat->cat_name;
+							} ?>
+					</span>
+					<?php if (has_post_thumbnail()) : ?>
+						<?php the_post_thumbnail(); ?>
+					<?php endif; ?>
+					<div class="excerpt">
+						<?php the_excerpt(); ?>
+					</div>
+				</a>
+		<?php endforeach;
+		endif;
+		wp_reset_postdata(); ?>
+
+</section>
+
 <div class="top-box col-xs-12">
 
 	<div class="page__contents-container">
@@ -16,7 +49,7 @@
 			$myposts = get_posts($args);
 			if ($myposts) :
 			?>
-				<h2 class="random-blog__sec-ttl">雑記ブログ</h2>
+				<h2 class="random-blog__sec-ttl">過去記事アーカイブ</h2>
 				<?php
 				foreach ($myposts as $post) :
 					setup_postdata($post);
