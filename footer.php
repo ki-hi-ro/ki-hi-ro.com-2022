c2ebc7547e2eb131fa5b110825fc24ddf2585705
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-08-12 08:12:54 +0900

    フッター

--- footer.php
+++ footer.php
@@ -3,12 +3,11 @@
 <footer>
 	<nav>
 		<div class="nav-contents">
-			<ul>
-				<li><a href="<?php echo home_url('#contact'); ?>">お問い合わせ</a></li>
-			</ul>
+			<h4><a href="<?php echo home_url('#contact'); ?>">お問い合わせ</a></h4>
 		</div>
 
-				<div class="nav-contents">
+		<div class="nav-contents">
+			<h4>仕事</h4>
 			<ul>
 				<li><a href="<?php echo home_url('#what-i-can'); ?>">対応可能な項目</a></li>
 				<li><a href="<?php echo home_url('#what-i-did'); ?>">これまでの仕事</a></li>
@@ -16,10 +15,11 @@
 		</div>
 
 		<div class="nav-contents">
+			<h4><a href="<?php echo home_url('#blog'); ?>">ブログ</a></h4>
 			<ul>
-				<li><a href="<?php echo home_url('#skill-blog'); ?>">技術ブログ</a></li>
-				<li><a href="<?php echo home_url('#study-blog'); ?>">学習ブログ</a></li>
-				<li><a href="<?php echo home_url('#random-blog'); ?>">雑記ブログ</a></li>
+				<li><a href="<?php echo home_url('category/skill-blog/'); ?>">技術ブログ</a></li>
+				<li><a href="<?php echo home_url('category/study-blog/'); ?>">学習ブログ</a></li>
+				<li><a href="<?php echo home_url('category/random-blog/'); ?>">雑記ブログ</a></li>
 			</ul>
 		</div>
 	</nav>
