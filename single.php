68dce4a68bee7b837628b060f8e56b0622e6451a
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-08-18 08:10:03 +0900

    投稿日を出力させた

--- single.php
+++ single.php
@@ -7,14 +7,15 @@
           <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
               <?php if(function_exists('bcn_display')) { bcn_display(); } ?>
           </div>
+          <?php if(have_posts()): ?>
+            <?php while(have_posts()): the_post(); ?>
           <div class="author calendar single-wrap">
+            <span class="post-date">投稿日: <?php the_date(); ?></span>
             <h1><?php the_title(); ?></h1>
             <div class="author__contents">
-              <?php if(have_posts()): ?>
-                <?php while(have_posts()): the_post(); ?>
                       <?php the_content(); ?>
-                  <?php endwhile; ?>
-              <?php endif; ?>
+            <?php endwhile; ?>
+          <?php endif; ?>
                 <?php
                   $my_query = new WP_Query();
                   global $post;
