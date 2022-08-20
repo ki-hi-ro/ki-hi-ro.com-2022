61d678a96b0c24df28e3824f04e7e073b8012e04
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-08-21 07:23:00 +0900

    自己紹介ページのテンプレ

---
+++ page-introduce.php
@@ -0,0 +1,51 @@
+<?php get_header(); ?>
+
+<div class="top-box col-xs-12">
+  <figure class="page-mv">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/introduce.png" alt="自己紹介 メイン画像">
+		<h1>自己紹介</h1>
+  </figure>
+
+	<section class="author" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+		<img src="https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/uploads/2022/01/2022-profile.png" class="img-responsive img-circle">
+		<h4>
+			<span itemprop="name">Shibata Hiroki</span>
+		</h4>
+		<hr>
+	</section>
+
+	<section class="message" id="message">
+		<h2 class="message__ttl">自分が世の中に提供できる価値は何か？<br>現時点での答えはWEB制作です。</h2>
+
+		<p>初めまして。柴田浩貴です。28歳です。大学は東京農業大学の造園学科でした。<br>緑に関わることをやっていきたいという思いで、進路選択をしました。<br>在学中は、塾講師で主に英語を教えていました。英語は昔から得意科目で、<br>センター試験では190点獲得し、英語検定準1級を持っています。</p>
+
+		<p>大学卒業後は、とあるビジネスホテルの新卒社員として約1年8ヶ月間、勤務しました。<br>得意な英語や塾講師で培ったホスピタリティー精神が活かせると考えたためです。<br>しかし、社員という責任ある立場で、夜勤が増えていったことから、睡眠不足を感じていました。<br>健康が第一優先と考えたため退職を決意。</p>
+
+		<p>その後は、札幌市図書情報館という蔵書が素晴らしい図書館で哲学・心理学などの<br>様々な本を読み漁りました。そして、起業家を紹介する雑誌で、Progateの代表が紹介されているのを発見しました。それがプログラミングとの出会いです。家に帰って自宅にあったiMacで早速ProgateのHTML / CSSコースをやってみました。頭が痛くなったことを覚えています。</p>
+
+		<p>会社を辞めて収入が途絶えたため、一旦実家に戻りました。<br>その頃、YouTubeでマナブさんやマコなり社長の動画をよく見ていました。<br>２人ともプログラミングを経験していました。</p>
+
+		<p>私もプログラミングに挑戦してみようと思い、名古屋のTECH::EXPERTで、70日間短期集中でRuby on Railsを使ったWebアプリケーションのアジャイル開発を経験しました。<br>その後、東京の会社にWEBエンジニアとして就職するのが一般的な流れでしたが、<br>私は、フリーランスという働き方に憧れを持っていたため、WEB制作のフリーランスという道を選びました。</p>
+
+		<p>実績や信頼が重要なフリーランスにとって、最初の仕事を獲得するまでのハードルがありましたが、なんとかお仕事をいただけて、WordPressサイトの制作や更新、EC-CUBEサイトの更新、マーケティング部署でのコーディング業務などを経験しました。</p>
+
+	</section>
+
+	<section class="address" id="address">
+		<h2 class="address__ttl">自宅兼事務所</h2>
+		<iframe class="pc-g-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.162785317019!2d136.8825941152762!3d35.17749506504015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600376c221ac3e83%3A0x740a7d00a14742b4!2z44CSNDUxLTAwNDUg5oSb55-l55yM5ZCN5Y-k5bGL5biC6KW_5Yy65ZCN6aeF77yS5LiB55uu77yV4oiS77yUIO-8te-8te-8suOCs-ODvOODiOWQjeWPpOWxi-WQjemnhQ!5e0!3m2!1sja!2sjp!4v1650798359378!5m2!1sja!2sjp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+		<iframe class="sp-g-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.162785317019!2d136.8825941152762!3d35.17749506504015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600376c221ac3e83%3A0x740a7d00a14742b4!2z44CSNDUxLTAwNDUg5oSb55-l55yM5ZCN5Y-k5bGL5biC6KW_5Yy65ZCN6aeF77yS5LiB55uu77yV4oiS77yUIO-8te-8te-8suOCs-ODvOODiOWQjeWPpOWxi-WQjemnhQ!5e0!3m2!1sja!2sjp!4v1650798359378!5m2!1sja!2sjp" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+		<h3>住所</h3>
+		<p>〒451-0045<br>愛知県名古屋市西区名駅2丁目5-4 UURコート名古屋名駅1504</p>
+		<h3>最寄駅</h3>
+		<p>名古屋駅</p>
+	</section>
+
+	<section class="twitter" id="twitter">
+		<h2 class="twitter__ttl">Twitter</h2>
+		<a class="twitter-timeline" data-width="400" data-height="1200" href="https://twitter.com/2021_shibata?ref_src=twsrc%5Etfw">Tweets by 2021_shibata</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+	</section>
+
+</div>
+
+<?php get_footer();?>
