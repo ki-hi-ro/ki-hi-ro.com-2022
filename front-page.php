0af9fc580db37263f52e471adf32326acc5fc29f
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-08-23 08:12:37 +0900

    文字なし画像を配置した

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php get_header();?>
 
 <div class="mv">
-    <img class="pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-pc.png?20220815"
+    <img class="pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-pc.png?20220823-1"
         alt="PCのメインビジュアル">
-    <img class="sp-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-sp.png?202208015"
+    <img class="sp-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-sp.png?20220823-1"
         alt="スマホのメインビジュアル">
     <div class="scrolldown1"><span>Scroll</span></div>
 </div>
