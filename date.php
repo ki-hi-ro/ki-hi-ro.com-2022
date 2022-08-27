4cc45bd4ebb9172ee445f54b3c2801aef994973a
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-08-27 11:48:13 +0900

    8月の投稿一覧からサンプルを削除した

--- date.php
+++ date.php
@@ -3,9 +3,15 @@
 <main class="container">
   <h1><?php echo get_the_date('Y年n月'); ?>の記事一覧</h1>
 	<section class="top-section sidebar__blog sidebar__contents">
-    <?php if(have_posts()): while(have_posts()):the_post(); ?>
+    <?php
+    $args = array(
+        'tag__not_in'   => array( 105 ),
+    );
+    $query = new WP_Query( $args );
+    ?>
+    <?php if(have_posts()): while($query->have_posts()):$query->the_post(); ?>
 	    <?php echo get_template_part('template-parts/sidebar-blog'); ?>
-    <?php endwhile; endif; ?>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
 	</section>
 
 </main>
