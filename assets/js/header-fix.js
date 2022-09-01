1d058515166052267032223d3a8f0017cd2a76ad
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-02 08:11:38 +0900

    page-linkの処理を削除した

--- assets/js/header-fix.js
+++ assets/js/header-fix.js
@@ -15,11 +15,4 @@ jQuery(function ($) {
   $(window).on('scroll', function () {
     FixedAnime();
   });
-
-  $nav_link.on('click', function () {
-    var elmHash = $(this).attr("href");
-    var pos = Math.round($(elmHash).offset().top - 200);
-    $("body,html").animate({ scrollTop: pos }, 1000);
-    return false;
-  });
 })
