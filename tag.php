79a3e6e8f5d2c12daf7d0a631d089b69f4e6a656
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-07 08:18:29 +0900

    タグ一覧のヘッダーが被ってしまっていた問題を解決した

--- tag.php
+++ tag.php
@@ -1,32 +1,42 @@
-<?php get_header();?>
+<?php get_header(); ?>
+
+<?php
+$term = get_queried_object();
+$term_slug = $term->slug;
+$term_name = $term->name;
+$term_desc = $term->description;
+?>
+
+<div class="mv">
+  <div class="mv-text-wrap">
+    <div class="mv-text"><?php echo $term_name; ?></div>
+  </div>
+  <img class="pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-pc.png?20220823-1" alt="PCのメインビジュアル">
+  <img class="sp-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-sp.png?20220823-1" alt="スマホのメインビジュアル">
+  <div class="scrolldown1"><span>Scroll</span></div>
+</div>
 
 <main class="container">
-  <?php
-  $term = get_queried_object();
-  $term_slug = $term->slug;
-  $term_name = $term->name;
-  $term_desc = $term->description;
-  ?>
 
-	<section class="top-section sidebar__blog sidebar__contents">
-		<h4><?php echo $term_name; ?>の記事一覧</h4>
-		<p><?php echo $term_desc; ?></p>
-  <?php
-  $args = array(
-    'posts_per_page' => -1,
-    'tag' => $term_slug
-  );
-  $the_query = new WP_Query($args);
-  if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-		<?php echo get_template_part('template-parts/sidebar-blog'); ?>
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
-    <?php else: ?>
-	    <h2>記事がありません</h2>
+  <section class="top-section sidebar__blog sidebar__contents">
+    <h4><?php echo $term_name; ?>の記事一覧</h4>
+    <p><?php echo $term_desc; ?></p>
+    <?php
+    $args = array(
+      'posts_per_page' => -1,
+      'tag' => $term_slug
+    );
+    $the_query = new WP_Query($args);
+    if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+        <?php echo get_template_part('template-parts/sidebar-blog'); ?>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+    <?php else : ?>
+      <h2>記事がありません</h2>
     <?php endif; ?>
 
-	</section>
+  </section>
 
 </main>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
