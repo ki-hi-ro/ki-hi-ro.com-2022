f6e4f15e1675dbe20e9de5b4ca196ef5dfe1ace9
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-07 08:27:06 +0900

    ブログ一覧のテンプレートパーツファイル名を変更した

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $term_desc = $term->description;
 		);
 		$myposts = get_posts($args);
 		foreach ($myposts as $post) : setup_postdata($post); ?>
-			<?php echo get_template_part('template-parts/sidebar-blog'); ?>
+			<?php echo get_template_part('template-parts/blog-list'); ?>
 		<?php
 		endforeach;
 		wp_reset_postdata();
@@ -38,13 +38,9 @@ $term_desc = $term->description;
 
 	<h4 class="tag-ttl">タグ</h4>
 	<?php
-	// カテゴリーアーカイブの場合
 	if (is_category()) {
-		// 現在のカテゴリーのIDを取得
 		$cat_id = get_query_var('cat');
-		// 作成した関数からタグのオブジェクトを取得
 		$posttags = my_tags_in_cat($cat_id);
-		// タグのオブジェクトを利用しタグ一覧を作成
 		if ($posttags) {
 			echo '<ul class="tag-list">';
 			foreach ($posttags as $tag) {
