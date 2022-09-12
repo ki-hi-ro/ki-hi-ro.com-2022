6b4194446371abefd492c07b5e974087aa4c1ba4
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-12 09:02:12 +0900

    これまでの仕事の崩れを修正した

--- page-what-i-did.php
+++ page-what-i-did.php
@@ -12,17 +12,16 @@
   <section class="top-section" id="what-i-did">
     <h4>これまでの仕事</h4>
     <p>これまでにクラウドソーシングや、エージェント、地元のWEB制作会社経由で様々なお仕事を経験しました。どんな仕事をしてきたのかを、掲載可能な範囲で投稿していきます。</p>
-    <div class="top-contents__wrap">
+
+    <ul class="blog-list-grid">
       <?php $args = array('category_name' => 'what-i-did', 'post_type' => 'post', 'posts_per_page' => -1,);
       $myposts = get_posts($args);
-      foreach ($myposts as $post) : setup_postdata($post); ?>
-        <?php echo get_template_part('template-parts/top-contents'); ?>
+      ?>
+      <?php foreach ($myposts as $post) : setup_postdata($post); ?>
+        <?php echo get_template_part('template-parts/blog-list-grid'); ?>
       <?php endforeach;
       wp_reset_postdata(); ?>
-    </div>
-    <div class="more-link --top-sec">
-      <a href="<?php echo home_url('category/what-i-did'); ?>">これまでの仕事一覧<img src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg" /></a>
-    </div>
+    </ul>
   </section>
 </main>
 
