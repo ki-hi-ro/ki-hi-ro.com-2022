722738af333f6d3ab20fbad0ea016519b0b1cc36
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-12 09:17:34 +0900

    日付とタグ一覧をgridレイアウトにした

--- date.php
+++ date.php
@@ -12,22 +12,24 @@
 
   <h1><?php echo get_the_date('Y年n月'); ?>の記事一覧</h1>
   <section class="top-section sidebar__blog sidebar__contents">
-    <?php
-    $current_url =  get_pagenum_link(get_query_var('paged'));
-    $uri = rtrim($current_url, '/');
-    $uri = substr($uri, strrpos($uri, '/') + 1);
-    $uri = abs($uri);
-    $args = array(
-      'tag__not_in'   => array(116),
-      'monthnum' => $uri,
-    );
-    $query = new WP_Query($args);
-    ?>
-    <?php if (have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-        <?php echo get_template_part('template-parts/sidebar-blog'); ?>
-    <?php endwhile;
-    endif;
-    wp_reset_postdata(); ?>
+    <ul class="blog-list-grid">
+      <?php
+      $current_url =  get_pagenum_link(get_query_var('paged'));
+      $uri = rtrim($current_url, '/');
+      $uri = substr($uri, strrpos($uri, '/') + 1);
+      $uri = abs($uri);
+      $args = array(
+        'tag__not_in'   => array(116),
+        'monthnum' => $uri,
+      );
+      $query = new WP_Query($args);
+      ?>
+      <?php if (have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+          <?php echo get_template_part('template-parts/blog-list-grid'); ?>
+      <?php endwhile;
+      endif;
+      wp_reset_postdata(); ?>
+    </ul>
   </section>
 
 </main>
--- tag.php
+++ tag.php
@@ -21,19 +21,21 @@ $term_desc = $term->description;
   <section class="top-section sidebar__blog sidebar__contents">
     <h4><?php echo $term_name; ?>の記事一覧</h4>
     <p><?php echo $term_desc; ?></p>
-    <?php
-    $args = array(
-      'posts_per_page' => -1,
-      'tag' => $term_slug
-    );
-    $the_query = new WP_Query($args);
-    if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-        <?php echo get_template_part('template-parts/sidebar-blog'); ?>
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?>
-    <?php else : ?>
-      <h2>記事がありません</h2>
-    <?php endif; ?>
+    <ul class="blog-list-grid">
+      <?php
+      $args = array(
+        'posts_per_page' => -1,
+        'tag' => $term_slug
+      );
+      $the_query = new WP_Query($args);
+      if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+          <?php echo get_template_part('template-parts/blog-list-grid'); ?>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+      <?php else : ?>
+        <h2>記事がありません</h2>
+      <?php endif; ?>
+    </ul>
 
   </section>
 
