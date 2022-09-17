6fdb63b81cb369f336d59551bc9d38523052e06f
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-17 18:12:02 +0900

    技術ブログの処理を関数にした

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,39 +1,43 @@
-const topBlogTag_skill = document.querySelectorAll(".top-skill-blog__tag-li");
-const topBlogTagContent_skill = document.querySelectorAll(".blog-list-scroll__link--skill-blog");
+const tag_list_skil = document.querySelectorAll(".top-skill-blog__tag-li");
+const article_list_skil = document.querySelectorAll(".blog-list-scroll__link--skill-blog");
 
-window.addEventListener(
-  "load",
-  function () {
-    topBlogTag_skill.forEach(function (tag) {
+tag_motion(tag_list_skil, article_list_skil);
+
+function tag_motion(tag_list, article_list) {
+  window.addEventListener(
+    "load",
+    function () {
+      tag_list.forEach(function (tag) {
+        tag_contentid = tag.getAttribute("contentid");
+        if (tag_contentid == "html-css") {
+          tag.classList.add("active");
+        }
+      });
+
+      article_list.forEach(function (article) {
+        if (!article.classList.contains("html-css")) {
+          article.classList.add("notShowMe");
+        }
+      });
+    },
+    false
+  );
+
+  tag_list.forEach(function (tag) {
+    tag.addEventListener("click", function () {
+      const allTag = document.querySelectorAll(".top-skill-blog__tag-li");
+      allTag.forEach(function (oneTag) {
+        oneTag.classList.remove("active");
+      });
+      tag.classList.add("active");
+
       tag_contentid = tag.getAttribute("contentid");
-      if (tag_contentid == "html-css") {
-        tag.classList.add("active");
-      }
-    });
-
-    topBlogTagContent_skill.forEach(function (article) {
-      if (!article.classList.contains("html-css")) {
-        article.classList.add("notShowMe");
-      }
-    });
-  },
-  false
-);
-
-topBlogTag_skill.forEach(function (tag) {
-  tag.addEventListener("click", function () {
-    const allTag = document.querySelectorAll(".top-skill-blog__tag-li");
-    allTag.forEach(function (oneTag) {
-      oneTag.classList.remove("active");
-    });
-    tag.classList.add("active");
-
-    tag_contentid = tag.getAttribute("contentid");
-    topBlogTagContent_skill.forEach(function (article) {
-      article.classList.remove("notShowMe");
-      if (!article.classList.contains(tag_contentid)) {
-        article.classList.add("notShowMe");
-      }
+      article_list.forEach(function (article) {
+        article.classList.remove("notShowMe");
+        if (!article.classList.contains(tag_contentid)) {
+          article.classList.add("notShowMe");
+        }
+      });
     });
   });
-});
+}
