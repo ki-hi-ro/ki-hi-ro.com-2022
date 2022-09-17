e5c63b3e8e8f3289b267010699bc3f96be38715d
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-17 13:50:50 +0900

    デフォルトのタグをアクティブにした

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -4,6 +4,13 @@ const topBlogTagContent_skill = document.querySelectorAll(".blog-list-scroll__li
 window.addEventListener(
   "load",
   function () {
+    topBlogTag_skill.forEach(function (tag) {
+      tag_contentid = tag.getAttribute('contentid');
+      if (tag_contentid == 'html-css') {
+        tag.classList.add('active');
+      }
+    });
+
     topBlogTagContent_skill.forEach(function (article) {
       if (!article.classList.contains('html-css')) {
         article.classList.add('notShowMe');
