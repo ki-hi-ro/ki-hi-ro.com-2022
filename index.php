c228a60405efa1c6ca6c76d2fc504ab455f4f782
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-20 13:57:45 +0900

    背景画像を作成した

--- index.php
+++ index.php
@@ -4,14 +4,25 @@ $term = get_queried_object();
 $term_slug = $term->slug;
 $term_name = $term->name;
 $term_desc = $term->description;
+$mv_pc = 'mv-pc';
+if ($term_name == '技術ブログ') {
+	$mv_pc = 'mv-pc-skill';
+} else if ($term_name == '学習ブログ') {
+	$mv_pc = 'mv-pc-study';
+} else if ($term_name == '雑記ブログ') {
+	$mv_pc = 'mv-pc-random';
+}
 ?>
 <div class="blog-archive__mv mv">
 	<div class="blog-archive__mv-text-wrap mv-text-wrap">
 		<div class="blog-archive__mv-text mv-text"><?php echo $term_name; ?></div>
 	</div>
-	<img class="blog-archive__pc-img pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-pc.png?20220823-1" alt="PCのメインビジュアル">
+
+	<img class="blog-archive__pc-img pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/<?php echo $mv_pc; ?>.png?20220823-1" alt="PCのメインビジュアル">
 	<img class="blog-archive__sp-img sp-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-sp.png?20220823-1" alt="スマホのメインビジュアル">
-	<div class="scrolldown1"><span>Scroll</span></div>
+	<div class="blog-archive__scroll scrolldown1">
+		<span class="blog-archive__scroll-text">Scroll</span>
+	</div>
 </div>
 
 <main class="container">
