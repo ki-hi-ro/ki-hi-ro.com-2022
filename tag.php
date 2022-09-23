187af0a30c6e069d252a64f16494fab9ec79740b
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-23 11:24:37 +0900

    タグのスクロールを修正した

--- tag.php
+++ tag.php
@@ -7,14 +7,7 @@ $term_name = $term->name;
 $term_desc = $term->description;
 ?>
 
-<div class="mv">
-  <div class="mv-text-wrap">
-    <div class="mv-text"><?php echo $term_name; ?></div>
-  </div>
-  <img class="pc-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-pc.png?20220823-1" alt="PCのメインビジュアル">
-  <img class="sp-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/mv-sp.png?20220823-1" alt="スマホのメインビジュアル">
-  <div class="scrolldown1"><span>Scroll</span></div>
-</div>
+<?php echo get_template_part('template-parts/mv'); ?>
 
 <main class="container">
 
--- template-parts/mv.php
+++ template-parts/mv.php
@@ -27,12 +27,16 @@ if ($term_name == '技術ブログ') {
 if (is_front_page() || is_home()) {
   $mv_text = 'WEB制作のコーディングで<span class="sp-none">、</span><br class="only-sp">価値を提供する';
   $class_block = 'front-page';
-} else if (is_archive()) {
+} else if (is_archive() && !is_tag()) {
   $mv_text = $term_name;
   $class_block = 'blog-archive';
 } else if (is_page('what-i-did')) {
   $mv_text = 'これまでの仕事';
   $class_block = 'page-what-i-did';
+} else if (is_tag()) {
+  $mv_text = $term_name;
+  $class_block = 'tag';
+  $modify = '--tag';
 }
 ?>
 <div class="mv">
