bc19ab0213377629a06db2fbc0026c7313a94211
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-23 12:15:13 +0900

    date.phpのエラーを解決した

--- template-parts/mv.php
+++ template-parts/mv.php
@@ -1,28 +1,30 @@
 <?php
 if (!is_front_page() && !is_home()) {
   $term = get_queried_object();
-  $term_slug = $term->slug;
-  $term_name = $term->name;
-  $term_desc = $term->description;
   $mv_text = '';
   $mv_pc = 'mv-pc';
   $mv_sp = 'mv-sp';
-  if ($term_name == '技術ブログ') {
-    $mv_pc = 'mv-pc-skill';
-    $mv_sp = 'mv-sp-skill';
-    $modify = '--blog --skill';
-  } else if ($term_name == '学習ブログ') {
-    $mv_pc = 'mv-pc-study';
-    $mv_sp = 'mv-sp-study';
-    $modify = '--blog --study';
-  } else if ($term_name == '雑記ブログ') {
-    $mv_pc = 'mv-pc-random';
-    $mv_sp = 'mv-sp-random';
-    $modify = '--blog --random';
-  } else if ($term_name == 'これまでの仕事') {
-    $mv_pc = 'mv-pc';
-    $mv_sp = 'mv-sp';
-    $modify = '--what-i-did';
+  if(!is_date()) {
+    $term_slug = $term->slug;
+    $term_name = $term->name;
+    $term_desc = $term->description;
+    if ($term_name == '技術ブログ') {
+      $mv_pc = 'mv-pc-skill';
+      $mv_sp = 'mv-sp-skill';
+      $modify = '--blog --skill';
+    } else if ($term_name == '学習ブログ') {
+      $mv_pc = 'mv-pc-study';
+      $mv_sp = 'mv-sp-study';
+      $modify = '--blog --study';
+    } else if ($term_name == '雑記ブログ') {
+      $mv_pc = 'mv-pc-random';
+      $mv_sp = 'mv-sp-random';
+      $modify = '--blog --random';
+    } else if ($term_name == 'これまでの仕事') {
+      $mv_pc = 'mv-pc';
+      $mv_sp = 'mv-sp';
+      $modify = '--what-i-did';
+    }
   }
 } else {
   $mv_pc = 'mv-pc';
