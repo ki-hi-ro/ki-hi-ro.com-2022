f392a453c5d42fc5514f59ccb044916d64d52d51
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-09-27 09:02:03 +0900

    9月27日

--- template-parts/blog-list-scroll.php
+++ template-parts/blog-list-scroll.php
@@ -8,11 +8,12 @@ if ($posttags) : foreach ($posttags as $tag) :
         <div class="blog-list-scroll__contents">
             <div class="blog-list-scroll__thumb-nail-wrap">
                     <?php
-                    if (has_post_thumbnail()) : the_post_thumbnail('full', ['class' => 'blog-list-scroll__img']);
-                    else :
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('full', ['class' => 'blog-list-scroll__img']);
+                    } else {
                     ?>
                         <img class="blog-list-scroll__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image" />
-                    <?php endif; ?>
+                    <?php } ?>
             </div>
             <div class="blog-list-scroll__meta">
                     <span class="blog-list-scroll__date">
