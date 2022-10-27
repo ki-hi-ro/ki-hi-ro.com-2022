4eec7d2d344884d0dc01f3e4d88d1c043bbb74c7
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-10-27 11:15:10 +0900

    トップの構成変更中

--- template-parts/author.php
+++ template-parts/author.php
@@ -1,9 +1,9 @@
-<section class="author">
-  <img class="author__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-09.jpg?20220925">
-  <h4 class="author__name">Shibata Hiroki</h4>
-  <hr class="author__line">
-  <p class="author__text">自分のパフォーマンスを最大限に出せる時間帯と場所で働きたいと思ったため、フリーランスのWEB制作エンジニアになりました。2019年末にTECH::EXPERTを受講し、現在はWEB制作会社の担当者の方から定期的に仕事のご依頼を受けています。</p>
-  <div class="author__more">
-    <a class="author__link" href="<?php echo home_url('self-produce'); ?>">自己紹介<img src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg" /></a>
+<section class="author-box">
+  <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-09.jpg?20220925">
+  <h4 class="author-box__my-name">Shibata Hiroki</h4>
+  <hr class="author-box__line">
+  <p class="author-box__text">自分のパフォーマンスを最大限に出せる時間帯と場所で働きたいと思ったため、フリーランスのWEB制作エンジニアになりました。2019年末にTECH::EXPERTを受講し、現在はWEB制作会社の担当者の方から定期的に仕事のご依頼を受けています。</p>
+  <div class="author-box__more">
+    <a class="author-box__link" href="<?php echo home_url('self-produce'); ?>">自己紹介<img src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg" /></a>
   </div>
 </section>
