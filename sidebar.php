52060f5b50ec8507170d102fedc79e24dd41574e
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-10-28 19:17:39 +0900

    Twitterのタイムラインを埋め込んだ

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,10 @@
   <aside class="l-pc-right">
     <?php echo get_template_part('template-parts/author'); ?>
 
+    <div class="twitter">
+      <a class="twitter-timeline" data-width="100%" data-height="533" data-theme="light" href="https://twitter.com/2021_shibata?ref_src=twsrc%5Etfw">Tweets by 2021_shibata</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
     <div class="sidebar-archive">
       <h3 class="sidebar-archive__ttl sidebar-ttl">過去記事アーカイブ</h3>
       <ul class="sidebar-archive__list-wrap sidebar-archive-list">
