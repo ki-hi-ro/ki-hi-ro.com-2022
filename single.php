a3c55540043183bf0e0100ee25c1e2a674b820de
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-10-30 18:14:04 +0900

    個別ページのリファクタリング

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ $cat = $category[0]; ?>
       <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
           <div class="single-article__post author calendar single-wrap">
-            <div class="post-meta">
+            <div class="single-article__post-meta post-meta">
               <span class="post-date">投稿日: <?php echo get_the_date('Y.m.d'); ?></span>
               <span class="modify-date">更新日: <?php echo get_the_modified_date('Y.m.d'); ?></span>
             </div>
