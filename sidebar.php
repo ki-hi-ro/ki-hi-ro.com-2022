ef65c8b54d6bf4f401be8fb816e346d198892371
khiro <hiroki.hiroki@icloud.com>
2022-11-01 18:07:10 +0900

    Twitterを非表示にした

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
   <aside class="l-pc-right">
     <?php echo get_template_part('template-parts/author'); ?>
 
-    <div class="twitter">
+    <!-- <div class="twitter">
       <a class="twitter-timeline" data-width="100%" data-height="475" data-theme="light" href="https://twitter.com/2021_shibata?ref_src=twsrc%5Etfw">Tweets by 2021_shibata</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
+    </div> -->
 
     <div class="sidebar-archive">
       <h3 class="sidebar-archive__ttl sidebar-ttl">過去記事アーカイブ</h3>
