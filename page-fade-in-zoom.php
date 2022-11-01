222963d510a5e08aca8967c0b8d7cf4087329bd3
khiro <hiroki.hiroki@icloud.com>
2022-11-01 16:06:24 +0900

    名古屋で撮影した写真セクションを作成していく

--- page-fade-in-zoom.php
+++ page-fade-in-zoom.php
@@ -1,7 +1,17 @@
 <?php get_header();?>
 
 <div class="zoom-slider" id="slider">
-  <span class="zoom-slider__text">名古屋</span>
+  <h1 class="zoom-slider__text">名古屋</h1>
+</div>
+
+<div class="fadein">
+  <h2 class="fadein__ttl">名古屋で撮影した写真</h2>
+  <div class="fadein__contents">
+    <?php for( $i = 1; $i <= 6; $i++) : ?>
+	    <img class="fadein__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/fade-in-zoom/nagoya-0<?php echo $i; ?>.jpg" alt="名古屋の写真<?php echo $i; ?>">
+    <?php endfor; ?>
+  </div>
+</a>
 </div>
 
 <?php get_footer();?>
