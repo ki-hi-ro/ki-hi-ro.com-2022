26475cc0383bf783da6c090fac5e25c3d1a5518a
khiro <hiroki.hiroki@icloud.com>
2022-11-01 17:56:37 +0900

    名古屋のページがひとまず完成した

--- assets/js/fade-in-zoom.js
+++ assets/js/fade-in-zoom.js
@@ -25,3 +25,25 @@ jQuery("#slider").vegas({
   timer: false,
   shuffle: true,
 });
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime($trigger) {
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  jQuery($trigger).each(function () {
+    //fadeInUpTriggerというクラス名が
+    var elemPos = jQuery(this).offset().top; //要素より、50px上の
+    var scroll = jQuery(window).scrollTop();
+    var windowHeight = jQuery(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      jQuery(this).addClass("--fadeIn"); // 画面内に入ったらfadeInというクラス名を追記
+    } else {
+      jQuery(this).removeClass("--fadeIn"); // 画面外に出たらfadeInというクラス名を外す
+    }
+  });
+} //ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+jQuery(window).scroll(function () {
+  fadeAnime(".--fadeInTrigger"); /* アニメーション用の関数を呼ぶ*/
+  fadeAnime(".--fadeInTrigger-sp"); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
