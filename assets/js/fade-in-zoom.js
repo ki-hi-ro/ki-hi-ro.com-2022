c5d82e16cefe2b6f29f53b23478bf5c726676b0c
khiro <hiroki.hiroki@icloud.com>
2022-11-01 15:33:15 +0900

    メインビジュアルのズーム完了

--- assets/js/fade-in-zoom.js
+++ assets/js/fade-in-zoom.js
@@ -5,28 +5,32 @@ var windowwidth =
 if (windowwidth > 768) {
   var responsiveImage = [
     //PC用の画像
-    { src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/dainagoya.jpg" },
-    { src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/midland.jpg" }    
+    {
+      src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/dainagoya.jpg",
+    },
+    {
+      src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/midland.jpg",
+    },
   ];
-// } else {
-//   var responsiveImage = [
-//     //タブレットサイズ（768px）以下用の画像
-//     { src: "./img/img_sp_01.jpg" },
-//     { src: "./img/img_sp_02.jpg" },
-//     { src: "./img/img_sp_03.jpg" },
-//   ];
+  // } else {
+  //   var responsiveImage = [
+  //     //タブレットサイズ（768px）以下用の画像
+  //     { src: "./img/img_sp_01.jpg" },
+  //     { src: "./img/img_sp_02.jpg" },
+  //     { src: "./img/img_sp_03.jpg" },
+  //   ];
 }
 
 //Vegas全体の設定
 
 jQuery("#slider").vegas({
-  overlay: false, 
-  transition: "fade", 
-  transitionDuration: 3000, 
+  overlay: true,
+  transition: "fade",
+  transitionDuration: 3000,
   delay: 5000,
   animationDuration: 60000,
-  animation: "kenburns", 
-  slides: responsiveImage, 
-  timer:false,
-  shuffle:true,
+  animation: "kenburns",
+  slides: responsiveImage,
+  timer: false,
+  shuffle: true,
 });
--- page-fade-in-zoom.php
+++ page-fade-in-zoom.php
@@ -1,5 +1,7 @@
 <?php get_header();?>
 
-<div class="zoom-slider" id="slider"></div>
+<div class="zoom-slider" id="slider">
+  <span class="zoom-slider__text">名古屋</span>
+</div>
 
 <?php get_footer();?>
