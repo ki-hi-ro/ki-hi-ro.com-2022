4ba910397c9b941c9daf47558c19640bb1d55a79
khiro <hiroki.hiroki@icloud.com>
2022-11-01 16:44:12 +0900

    レスポンシブの設定を無くした

--- assets/js/fade-in-zoom.js
+++ assets/js/fade-in-zoom.js
@@ -2,24 +2,15 @@
 
 var windowwidth =
   window.innerWidth || document.documentElement.clientWidth || 0;
-if (windowwidth > 768) {
-  var responsiveImage = [
-    //PC用の画像
-    {
-      src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/dainagoya.jpg",
-    },
-    {
-      src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/midland.jpg",
-    },
-  ];
-  // } else {
-  //   var responsiveImage = [
-  //     //タブレットサイズ（768px）以下用の画像
-  //     { src: "./img/img_sp_01.jpg" },
-  //     { src: "./img/img_sp_02.jpg" },
-  //     { src: "./img/img_sp_03.jpg" },
-  //   ];
-}
+var responsiveImage = [
+  //PC用の画像
+  {
+    src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/dainagoya.jpg",
+  },
+  {
+    src: "https://ki-hi-ro.com/ki-hi-ro.com-2022/wp-content/themes/ki-hi-ro.com-2022/assets/img/fade-in-zoom/midland.jpg",
+  },
+];
 
 //Vegas全体の設定
 
