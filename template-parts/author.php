f7ff54138995e208893e113f20dbabff58be7ab2
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-11-02 17:20:44 +0900

    プロフィールのテキスト変更

--- template-parts/author.php
+++ template-parts/author.php
@@ -2,7 +2,7 @@
   <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-09.jpg?20220925">
   <h4 class="author-box__my-name">Shibata Hiroki</h4>
   <hr class="author-box__line">
-  <p class="author-box__text">2019年末、TECH::EXPERTでプログラミングを経験後に、WEB制作のフリーランスとして活動を続けてきました。<br>このブログでは、「これまでの仕事」「技術」「学習」「雑記」の4つのカテゴリーで発信していきます。</p>
+  <p class="author-box__text">2019年末、TECH::EXPERTでプログラミングを経験後に、WEB制作のフリーランスとして活動を続けてきました。<br>このブログでは、「これまでの仕事」「技術ブログ」「学習ブログ」「雑記ブログ」の4つのカテゴリーで発信していきます。</p>
   <div class="author-box__more">
     <a class="author-box__link" href="<?php echo home_url('self-produce'); ?>">自己紹介
       <img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg">
