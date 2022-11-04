187624c374d743eea9723a6ba04d9f31147f19e6
khiro <hiroki.hiroki@icloud.com>
2022-11-05 03:01:47 +0900

    画像名を条件分岐させた

--- page-nagoya.php
+++ page-nagoya.php
@@ -7,8 +7,8 @@
 <div class="fadein">
   <h2 class="fadein__ttl">名古屋で撮影した写真</h2>
   <div class="fadein__contents">
-    <?php for( $i = 1; $i <= 21; $i++) : ?>
-	    <img class="fadein__img --fadeIn --fadeInTrigger-sp <?php if($i > 3) : ?>--fadeInTrigger<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/img/fade-in-zoom/nagoya-0<?php echo $i; ?>.jpg" alt="名古屋の写真<?php echo $i; ?>">
+    <?php  for( $i = 1; $i <= 21; $i++) : ?>
+	    <img class="fadein__img --fadeIn --fadeInTrigger-sp <?php if($i > 3) : ?>--fadeInTrigger<?php endif; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/img/fade-in-zoom/nagoya-<?php if($i <= 9) : ?>0<?php endif; ?><?php echo $i; ?>.jpg" alt="名古屋の写真<?php echo $i; ?>">
     <?php endfor; ?>
   </div>
 </a>
