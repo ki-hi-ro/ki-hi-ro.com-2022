5b501d25429e4d25626963d1379e5f7ad94f3b76
khiro <hiroki.hiroki@icloud.com>
2022-11-04 10:32:49 +0900

    シンプルに記事内容のみ表示させた

--- page.php
+++ page.php
@@ -1,23 +1,6 @@
 <?php get_header(); ?>
-<main class="l-container">
-  <div class="l-pc-left">
-    <article class="single-article">
-      <div class="single-article__bread breadcrumbs">
-        <?php if (function_exists('bcn_display')) {
-          bcn_display();
-        } ?>
-      </div>
-      <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-
-              <?php the_content(); ?>
-        <?php endwhile; endif;
-          wp_reset_postdata();
-          ?>
-            </div>
-          </div>
-    </article>
-  </div>
-  <?php get_sidebar(); ?>
-</main>
+<?php if (have_posts()) : ?>
+<?php while (have_posts()) : the_post(); ?>
+<?php the_content(); ?>
+<?php endwhile; endif; wp_reset_postdata();?>
 <?php get_footer(); ?>
