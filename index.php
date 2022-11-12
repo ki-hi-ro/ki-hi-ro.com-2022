d22771a6c72f960cb73415647c62cf830134886d
khiro <hiroki.hiroki@icloud.com>
2022-11-12 11:38:38 +0900

    トップの新着記事を繰り返し表示できるようにした

--- index.php
+++ index.php
@@ -20,49 +20,29 @@
     }
     ?>
     
-    <h1 class="post-list-ttl --<?php echo $current_cat_slug; ?>"><?php echo $post_list_ttl; ?></h1>
-    <div class="new-article">
       <?php
-      $args = array(
-        'post_type' => 'post',
-        'posts_per_page' => 3,
-        'paged' => get_query_var('paged'),
-        'category' => $current_cat_id,
-        'post__not_in' => array(3874),
-      );
-      $myposts = get_posts($args);
-      ?>
-      <ul class="blog-list-grid">
-        <?php foreach ($myposts as $post): setup_postdata($post);?>
-	          <?php echo get_template_part('template-parts/blog-list-grid'); ?>
-	      <?php endforeach; wp_reset_postdata();?>
-      </ul>
-      <div class="author-box__more">
-        <a class="author-box__link" href="<?php echo home_url('all-article'); ?>">全ての記事を見る<img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg"></a>
+      $new_article_array = array(['全ての記事',''], ['WordPress','wordpress'], ['TypeScript','typescript']);
+      foreach ($new_article_array as $new_article) :
+        $args = array(
+          'post_type' => 'post',
+          'posts_per_page' => 3,
+          'paged' => get_query_var('paged'),
+          'tag' => $new_article[1]
+        );
+        $myposts = get_posts($args);
+        ?>
+      <div class="new-article">
+        <h1 class="post-list-ttl --<?php echo $current_cat_slug; ?>"><?php echo $new_article[0]; ?></h1>
+        <ul class="blog-list-grid">
+          <?php foreach ($myposts as $post): setup_postdata($post);?>
+              <?php echo get_template_part('template-parts/blog-list-grid'); ?>
+          <?php endforeach; wp_reset_postdata();?>
+        </ul>
+        <div class="author-box__more">
+          <a class="author-box__link" href="<?php echo home_url('all-article'); ?>">全ての記事を見る<img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg"></a>
+        </div>
       </div>
-    </div>
-
-    <h1 class="post-list-ttl --<?php echo $current_cat_slug; ?>"><?php echo $post_list_ttl; ?></h1>
-    <div class="new-article">
-      <?php
-      $args = array(
-        'post_type' => 'post',
-        'posts_per_page' => 3,
-        'paged' => get_query_var('paged'),
-        'category' => $current_cat_id,
-        'post__not_in' => array(3874),
-      );
-      $myposts = get_posts($args);
-      ?>
-      <ul class="blog-list-grid">
-        <?php foreach ($myposts as $post): setup_postdata($post);?>
-	          <?php echo get_template_part('template-parts/blog-list-grid'); ?>
-	      <?php endforeach; wp_reset_postdata();?>
-      </ul>
-      <div class="author-box__more">
-        <a class="author-box__link" href="<?php echo home_url('all-article'); ?>">全ての記事を見る<img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg"></a>
-      </div>
-    </div>
+     <?php endforeach; ?>
   </div>
   <?php get_sidebar();?>
 </main>
