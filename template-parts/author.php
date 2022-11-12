2ff9c13e7f69b5a23cda9c19c55411bc3f7b0aae
khiro <hiroki.hiroki@icloud.com>
2022-11-12 13:04:08 +0900

    プロフィール文の修正

--- template-parts/author.php
+++ template-parts/author.php
@@ -2,7 +2,7 @@
   <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-11-11.jpg?20221111">
   <h4 class="author-box__my-name">Shibata Hiroki</h4>
   <hr class="author-box__line">
-  <p class="author-box__text">2019年末、TECH::EXPERTでプログラミングを経験後に、WEB制作のフリーランスとして活動を続けてきました。<br>このブログでは、「これまでの仕事」「技術ブログ」「学習ブログ」「雑記ブログ」の4つのカテゴリーで発信していきます。</p>
+  <p class="author-box__text">2019年末、TECH::EXPERTでプログラミングを経験後に、WEB制作のフリーランスとして活動を続けてきました。<br><br>今後は、WEB開発のエンジニアとして就職したいです。現在は、Vue.jsを学んでいます。<br><br>また、日々の経験の質を高めるために飲み記録をはじめました。<br><br>お問い合わせがある方は、<br><a href="mailto:hiroki.hiroki@icloud.com">hiroki.hiroki@icloud.com</a>まで<br>お願いします。</p>
   <div class="author-box__more">
     <a class="author-box__link" href="<?php echo home_url('self-produce'); ?>">自己紹介
       <img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg">
