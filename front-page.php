60efd4cb17defb0df518a160e7f252c18a26a7bc
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-11-18 17:23:01 +0900

    ローディングの実装完了

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <div class="loading" id="splash">
   <div class="loading__logo-wrap" id="splash_logo">
-    <img class="loading__logo fadeUp" src="img/loading.svg" alt="">
+    <img class="loading__logo fadeUp" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/loading-icon.png" alt="ローディングアイコン">
   </div>
 </div>
 
