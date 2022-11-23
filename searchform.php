3f56afe612b9a1e309c2413f631f5ff1b5cd87bd
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-11-23 12:43:56 +0900

    不要な記述の削除

--- searchform.php
+++ searchform.php
@@ -1,8 +1,6 @@
 <form action="<?php echo esc_url(home_url('/')); ?>" method="get" class="search-form">
   <input type="text" name="s" placeholder="キーワードで検索する" id="s" class="search-form__input">
   <button type="submit" id="s" class="search-form__btn">
-    <span class="dashicons dashicons-search search-form__icon">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/search-icon.svg" alt="検索アイコン" class="search-form__icon-img">
-    </span>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/search-icon.svg" alt="検索アイコン" class="search-form__icon-img">
   </button>
 </form>
