983eed96e970458e9007fb6e2a6146789eb0d86b
khiro <hiroki.hiroki@icloud.com>
2022-12-06 17:53:13 +0900

    プロフィール文の変更

--- template-parts/author.php
+++ template-parts/author.php
@@ -1,9 +1,22 @@
 <section class="author-box">
-  <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-11-11.jpg?20221111">
-  <h4 class="author-box__my-name">Shibata Hiroki</h4>
-  <hr class="author-box__line">
-  <p class="author-box__text">現在、Vue.jsの学習中です。<br><br>お問い合わせがある方は、<br><a href="mailto:hiroki.hiroki@icloud.com">hiroki.hiroki@icloud.com</a>まで<br>お願いします。</p>
-  <!-- <div class="author-box__more">
+    <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-11-11.jpg?20221111">
+    <h4 class="author-box__my-name">Shibata Hiroki</h4>
+    <hr class="author-box__line">
+    <p class="author-box__text">
+        ▼ 個人サイトのリポジトリ<br>
+        <a href="https://github.com/ki-hi-ro/ki-hi-ro.com-2022">https://github.com/ki-hi-ro/ki-hi-ro.com-2022</a><br><br>
+
+        ※転職活動中です。<br><br>
+
+        ▼ 実務経験あり<br>
+        HTML / CSS（Sass）、JavaScript、jQuery、WordPress...など<br><br>
+
+        ▼ 学習中<br>
+        Vue.js、React、Django...など<br><br>
+
+        お問い合わせがある方は、<br><a href="mailto:hiroki.hiroki@icloud.com">hiroki.hiroki@icloud.com</a>まで<br>お願いします。
+    </p>
+    <!-- <div class="author-box__more">
     <a class="author-box__link" href="<?php echo home_url('self-produce'); ?>">自己紹介
       <img class="author-box__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/arrow-right.svg">
     </a>
