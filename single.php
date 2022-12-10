126fd6e94bf4618233416db9944f54c10dbff90f
khiro <hiroki.hiroki@icloud.com>
2022-12-10 19:27:25 +0900

    個別記事ページにタグを表示させた

--- single.php
+++ single.php
@@ -5,14 +5,28 @@
         <article class="post">
             <?php get_template_part('template-parts/bread'); ?>
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-              <?php get_template_part('template-parts/post/meta'); ?>
-              <h1 class="post__ttl"><?php the_title(); ?></h1>
-              <?php 
+            <?php get_template_part('template-parts/post/meta'); ?>
+            <div class="blog-list-grid__tag-wrap">
+                <?php
+                $posttags = get_the_tags();
+                if ($posttags) :
+                  foreach ($posttags as $tag) : 
+                ?>
+                    <a class="blog-list-grid__tag" href="<?php echo home_url('tag/'.$tag->slug.''); ?>">
+                        <?php echo $tag->name; ?>
+                    </a>
+                <?php 
+                  endforeach; 
+                endif;
+                ?>
+            </div>
+            <h1 class="post__ttl"><?php the_title(); ?></h1>
+            <?php 
               if(have_rows('what-you-know')): 
                 get_template_part('template-parts/what-you-know'); 
               endif; 
               ?>
-              <div class="post__content">
+            <div class="post__content">
                 <?php
                 if(!in_category('drink-comparison')) : 
                   the_content(); 
@@ -20,7 +34,7 @@
                   get_template_part('template-parts/drink-comparision'); 
                 endif; 
                 ?>
-              </div>
+            </div>
             <?php endwhile; endif; ?>
         </article>
     </div>
--- template-parts/blog-list-grid.php
+++ template-parts/blog-list-grid.php
@@ -15,10 +15,10 @@
         <?php
 			$posttags = get_the_tags();
 			if ($posttags) :
-			?>
+		?>
         <div class="blog-list-grid__tag-wrap">
             <?php foreach ($posttags as $tag) : ?>
-            <a class="blog-list-grid__tag" href="<?php echo home_url('tag/'.$tag->slug.''); ?>">            
+            <a class="blog-list-grid__tag" href="<?php echo home_url('tag/'.$tag->slug.''); ?>">
                 <?php echo $tag->name; ?>
             </a>
             <?php endforeach; ?>
@@ -26,18 +26,18 @@
         <?php endif; ?>
     </div>
     <?php if(have_rows('what-you-know')): ?>
-        <div class="blog-list-grid__what-you-know-ttl-wrap">    
-            <?php     
+    <div class="blog-list-grid__what-you-know-ttl-wrap">
+        <?php     
             $category = get_the_category();
             $cat_slug = $category[0]->category_nicename;  
             ?>
-            <h2 class="blog-list-grid__what-you-know-ttl <?php if($cat_slug): ?>--<?php echo $cat_slug; endif; ?>">この記事を読むと分かること</h2>
-            <ul>
-                <?php while(have_rows('what-you-know')): the_row(); ?>
-                <li class="blog-list-grid__what-you-know-li"><?php the_sub_field('text'); ?></li>
-                <?php endwhile; ?>
-            </ul>
-        </div>
+        <h2 class="blog-list-grid__what-you-know-ttl <?php if($cat_slug): ?>--<?php echo $cat_slug; endif; ?>">この記事を読むと分かること</h2>
+        <ul>
+            <?php while(have_rows('what-you-know')): the_row(); ?>
+            <li class="blog-list-grid__what-you-know-li"><?php the_sub_field('text'); ?></li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
     <?php endif; ?>
     <a class="blog-list-grid__link-btn" href="<?php the_permalink(); ?>">この記事を読みたい</a>
 </div>
