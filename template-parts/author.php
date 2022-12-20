83a487fe445b27b706e46623d280fdd6071fc6c2
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-12-20 14:47:56 +0900

    プロフィールにtwitterアイコンを追加した

--- template-parts/author.php
+++ template-parts/author.php
@@ -1,6 +1,13 @@
 <section class="author-box">
   <img class="author-box__img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/introduce/2022-11-11.jpg?20221111">
-  <h4 class="author-box__my-name">Shibata Hiroki</h4>
+  <h4 class="author-box__my-name">
+    Shibata Hiroki
+    <div class="header__li">
+      <a class="header__li-link" href="https://twitter.com/2021_shibata">
+        <img class="header__twitter" src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter.png" alt="twitterのアイコン">
+      </a>
+    </div>
+  </h4>
   <hr class="author-box__line">
   <p class="author-box__text">
     1993年生まれの29歳です。<br><br>
