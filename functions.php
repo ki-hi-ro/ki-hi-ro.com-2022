6aba0a5ff389741efc02f0b1a81a344eea82e68f
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-12-20 14:27:03 +0900

    使用していないsassファイルの削除

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@ wp_enqueue_style('single', get_template_directory_uri() . '/assets/css/single.cs
 wp_enqueue_style('layout', get_template_directory_uri() . '/assets/css/layout.css', [], date("YmdHi"));
 wp_enqueue_style('front-page', get_template_directory_uri() . '/assets/css/front-page.css', [], date("YmdHi"));
 
-wp_enqueue_style('mv', get_template_directory_uri() . '/assets/css/mv.css', [], date("YmdHi"));
 wp_enqueue_style('top-contact', get_template_directory_uri() . '/assets/css/top-contact.css', [], date("YmdHi"));
 wp_enqueue_style('news-ticker', 'https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css', [], date("YmdHi"));
 wp_enqueue_style('top-news', get_template_directory_uri() . '/assets/css/top-news.css', [], date("YmdHi"));
