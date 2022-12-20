6549453c753c8d597fdbd53c01e0101cd35b88fb
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-12-20 13:13:30 +0900

    プロフィール文の変更

--- template-parts/author.php
+++ template-parts/author.php
@@ -3,26 +3,10 @@
     <h4 class="author-box__my-name">Shibata Hiroki</h4>
     <hr class="author-box__line">
     <p class="author-box__text">
-        1993年生まれの29歳です。<br>
-        東京農業大学地域環境科学部造園科学科を卒業後に、アパホテル株式会社に就職しました。<br><br>
-        その後、プログラミングスクールTEXH::EXPERTでプログラミングを経験して、WEB制作のエンジニアとして、派遣社員やフリーランスなどを経験しました。<br><br>
-
-        ▼ WordPressのオリジナルテーマで作成された当サイトのリポジトリ<br>
-
-        <a href="https://github.com/ki-hi-ro/ki-hi-ro.com-2022" target="new">https://github.com/ki-hi-ro/ki-hi-ro.com-2022</a><br><br>
-        今は、WEBアプリケーション開発のエンジニアを目指して転職活動中です。<br>インフラエンジニアなども視野に入れています。<br><br>
-
-        ▼ 実務経験あり<br>
-        HTML / CSS（Sass）, PHP, JavaScript、jQuery、WordPress, EC-CUBE, Git（Gitlab）, chatwork, slack, googleスプレッドシート, AdobeXD・Photoshop・Illustratorからのコーディング（デザイン不可）...など<br><br>
-
-        ▼ 学習経験あり<br>
-        Haml、Ruby on Rails、Ruby, AWS（EC2, S3)...など<br><br>
-
-        ▼ 学習中<br>
-        Vue.js、React、Django, CI / CDツール...など<br><br>
-
-        お問い合わせがある方は、<br><a href="mailto:hiroki.hiroki@icloud.com">hiroki.hiroki@icloud.com</a>まで<br>お願いします。<br>
-        ※履歴書・職務経歴書は準備してあります。
+        1993年生まれの29歳です。<br><br>
+        ソフトウェア開発のエンジニアとして、<br>2月1日から就業開始します。<br><br>
+        このサイトでは、自分が体験したことなどをアウトプットしていきます。<br><br>
+        お問い合わせがある方は、<br><a href="mailto:hiroki.hiroki@icloud.com">hiroki.hiroki@icloud.com</a>まで<br>お願いします。
     </p>
     <!-- <div class="author-box__more">
     <a class="author-box__link" href="<?php echo home_url('self-produce'); ?>">自己紹介
