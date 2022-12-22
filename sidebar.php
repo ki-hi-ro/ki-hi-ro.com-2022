aca0706b14af3f1a9e7c5d128e8df75ac2372ba9
khiro <hiroki.hiroki@icloud.com>
2022-12-22 21:36:44 +0900

    サイドバーのタグを記事ページでは削除した

--- sidebar.php
+++ sidebar.php
@@ -10,24 +10,26 @@
       </ul>
     </div>
 
-    <div class="tag-list">
-      <h3 class="tag-list__ttl">タグ</h3>
-      <ul class="tag-list__wrap">
-        <?php
-    		$tags = get_tags();
-        foreach( $tags as $tag) {
-          if($tag->name != 'サンプル') {
-            echo '
-              <li class="tag-li">
-                <a href="'. get_tag_link($tag->term_id) .'" class="tag-li__link">
-                  ' . $tag->name . '
-                </a>
-              </li>';
+    <?php if(!is_single()) : ?>
+      <div class="tag-list">
+        <h3 class="tag-list__ttl">タグ</h3>
+        <ul class="tag-list__wrap">
+          <?php
+          $tags = get_tags();
+          foreach( $tags as $tag) {
+            if($tag->name != 'サンプル') {
+              echo '
+                <li class="tag-li">
+                  <a href="'. get_tag_link($tag->term_id) .'" class="tag-li__link">
+                    ' . $tag->name . '
+                  </a>
+                </li>';
+            }
           }
-        }
-        ?>
-      </ul>
-    </div>
+          ?>
+        </ul>
+      </div>
+    <?php endif; ?>
 
     <?php if(is_single()) : ?>
     <div class="blog-article__aside-table-of-contents">
