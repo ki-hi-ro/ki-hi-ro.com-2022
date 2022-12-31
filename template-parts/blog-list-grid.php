e21edf66b4e352ae7ba3c5530d9e4d1954cba3fc
khiro <hiroki.hiroki@icloud.com>
2022-12-31 15:09:41 +0900

    サムネをリンクにした

--- template-parts/blog-list-grid.php
+++ template-parts/blog-list-grid.php
@@ -1,11 +1,13 @@
 <div class="blog-list-grid__contents">
-    <div class="blog-list-grid__thumb-nail-wrap">
-        <?php
-			if (has_post_thumbnail()) : the_post_thumbnail('full', ['class' => 'blog-list-grid__img']);
-			else :
-			?>
-        <img class="blog-list-grid__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image" />
-        <?php endif; ?>
+    <a class="blog-list-grid__thumb-link" href="<?php the_permalink(); ?>">
+        <div class="blog-list-grid__thumb-nail-wrap">
+            <?php
+                if (has_post_thumbnail()) : the_post_thumbnail('full', ['class' => 'blog-list-grid__img']);
+                else :
+                ?>
+            <img class="blog-list-grid__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image" />
+            <?php endif; ?>
+    </a>
     </div>
     <div class="blog-list-grid__meta">
         <span class="blog-list-grid__date">
