6f7872681f4aff15e944432ada3973a803ed84cd
khiro <hiroki.hiroki@icloud.com>
2023-01-04 09:47:59 +0900

    footer.cssの削除

--- functions.php
+++ functions.php
@@ -2,10 +2,8 @@
 
 // css
 wp_enqueue_style('style', get_template_directory_uri() . '/style.css', [], date("YmdHi"));
-
 wp_enqueue_style('vegas', 'https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css', [], date("YmdHi"));
 
-wp_enqueue_style('footer', get_template_directory_uri() . '/assets/css/footer.css', [], date("YmdHi"));
 
 // js
 wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/6cff6feef5.js', null, null, true);
