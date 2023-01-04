2aa692910ec75098815725b34e0e9bda274aacf9
khiro <hiroki.hiroki@icloud.com>
2023-01-04 09:24:11 +0900

    related-post.cssの削除

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 // css
 wp_enqueue_style('style', get_template_directory_uri() . '/style.css', [], date("YmdHi"));
 
-wp_enqueue_style('related-post', get_template_directory_uri() . '/assets/css/related-post.css', [], date("YmdHi"));
 wp_enqueue_style('vegas', 'https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css', [], date("YmdHi"));
 wp_enqueue_style('fade-in-zoom', get_template_directory_uri() . '/assets/css/fade-in-zoom.css', [], date("YmdHi"));
 wp_enqueue_style('list-ttl', get_template_directory_uri() . '/assets/css/list-ttl.css', [], date("YmdHi"));
