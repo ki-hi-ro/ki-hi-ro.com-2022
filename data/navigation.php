cac2f642f1a20e3af2eff75046fd721defbe2750
khiro <hiroki.hiroki@icloud.com>
2023-01-05 14:35:56 +0900

    振り返りと目標を追加した

--- data/navigation.php
+++ data/navigation.php
@@ -1,6 +1,6 @@
 <?php
 $nav = [
-    [ 'ttl' => ['','カテゴリー'], 'child' => [ ['/category/random-blog','雑記ブログ'], ['/category/study-blog', '学習ブログ'], ['/category/skill-blog', '技術ブログ'], ['/category/what-i-did', 'これまでの仕事'], ['/category/record', '記録'] ] ],
+    [ 'ttl' => ['','カテゴリー'], 'child' => [ ['/category/random-blog','雑記ブログ'], ['/category/study-blog', '学習ブログ'], ['/category/skill-blog', '技術ブログ'], ['/category/what-i-did', 'これまでの仕事'], ['/category/record', '記録'], ['/category/retrospect-and-goals/', '振り返りと目標'] ] ],
     [ 'ttl' => ['/tag/サイト制作', 'サイト制作'], 'child' => [ ['/tag/html-css','HTML / CSS'], ['/tag/wordpress', 'WordPress'], ['/tag/jquery', 'jQuery'] ] ],
     [ 'ttl' => ['/tag/プログラミング', 'プログラミング'], 'child' => [ ['/tag/java','Java'], ['/tag/vue-js', 'Vue.js'], ['/tag/typescript', 'TypeScript'] ] ],
 ];
