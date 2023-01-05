09e66594027487bf08cae7356c359df2676f7b23
khiro <hiroki.hiroki@icloud.com>
2023-01-05 17:21:05 +0900

    ブログリストグリッドのリファクタリング

--- template-parts/blog-list-grid.php
+++ template-parts/blog-list-grid.php
@@ -1,15 +1,16 @@
 <div class="blog-list-grid__contents">
-    <a class="blog-list-grid__thumb-link" href="<?php the_permalink(); ?>">
-        <div class="blog-list-grid__thumb-nail-wrap">
+    <div class="blog-list-grid__thumb-nail-wrap">
+        <a class="blog-list-grid__thumb-link" href="<?php the_permalink(); ?>">
             <?php
                 if (has_post_thumbnail()) : the_post_thumbnail('full', ['class' => 'blog-list-grid__img']);
                 else :
                 ?>
             <img class="blog-list-grid__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image" />
             <?php endif; ?>
-    </a>
+        </a>
     </div>
     <div class="blog-list-grid__meta">
+        <span class="blog-list-grid__category"></span>
         <span class="blog-list-grid__date">
             <span class="meta__date">投稿日: <?php echo get_the_date('Y.m.d'); ?></span>
             <span class="meta__date">更新日: <?php echo get_the_modified_date('Y.m.d'); ?></span>
@@ -27,19 +28,5 @@
         </div>
         <?php endif; ?>
     </div>
-    <?php if(have_rows('what-you-know')): ?>
-    <div class="blog-list-grid__what-you-know-ttl-wrap">
-        <?php
-            $category = get_the_category();
-            $cat_slug = $category[0]->category_nicename;
-            ?>
-        <h2 class="blog-list-grid__what-you-know-ttl <?php if($cat_slug): ?>--<?php echo $cat_slug; endif; ?>">この記事を読むと分かること</h2>
-        <ul class="blog-list-grid__what-you-know-ul">
-            <?php while(have_rows('what-you-know')): the_row(); ?>
-            <li class="blog-list-grid__what-you-know-li"><?php the_sub_field('text'); ?></li>
-            <?php endwhile; ?>
-        </ul>
-    </div>
-    <?php endif; ?>
     <a class="blog-list-grid__link-btn" href="<?php the_permalink(); ?>">この記事を読みたい</a>
 </div>
