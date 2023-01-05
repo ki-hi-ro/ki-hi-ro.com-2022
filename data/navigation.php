73efaf546fbe0e72710383a4d99bf412d9c66c5f
khiro <hiroki.hiroki@icloud.com>
2023-01-05 14:56:58 +0900

    プログラミングに項目を追加した

--- data/navigation.php
+++ data/navigation.php
@@ -2,6 +2,6 @@
 $nav = [
     [ 'ttl' => ['','カテゴリー'], 'child' => [ ['/category/random-blog','雑記ブログ'], ['/category/study-blog', '学習ブログ'], ['/category/skill-blog', '技術ブログ'], ['/category/what-i-did', 'これまでの仕事'], ['/category/record', '記録'], ['/category/retrospect-and-goals/', '振り返りと目標'] ] ],
     [ 'ttl' => ['/tag/サイト制作', 'サイト制作'], 'child' => [ ['/tag/html-css','HTML / CSS'], ['/tag/wordpress', 'WordPress'], ['/tag/jquery', 'jQuery'] ] ],
-    [ 'ttl' => ['/tag/プログラミング', 'プログラミング'], 'child' => [ ['/tag/java','Java'], ['/tag/vue-js', 'Vue.js'], ['/tag/typescript', 'TypeScript'] ] ],
+    [ 'ttl' => ['/tag/プログラミング', 'プログラミング'], 'child' => [ ['/tag/java','Java'], ['/tag/javascript','JavaScript'], ['/tag/react','React'], ['/tag/vue-js', 'Vue.js'], ['/tag/typescript', 'TypeScript'] ] ],
 ];
 ?>
