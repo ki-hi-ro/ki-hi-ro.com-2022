8c126e6ff768fa5dfe130bb8267a9904bd2f4f03
ki-hi-ro <hiroki.hiroki@icloud.com>
2023-02-06 22:21:57 +0900

    新着記事がトップに表示されるようにした

--- front-page.php
+++ front-page.php
@@ -4,6 +4,39 @@
 
 <main class="front-container">
   <section class="front-sec">
+    <h2 class="front-sec__ttl">新着記事</h2>
+    <div class="front-sec__text">
+      <?php
+      $news_query = new WP_Query(
+        array(
+          'post_type'      => 'post',
+          'posts_per_page' => 1,
+        )
+      );
+      ?>
+      <?php if ( $news_query->have_posts() ) : ?>
+      <?php while ( $news_query->have_posts() ) : ?>
+      <?php $news_query->the_post(); ?>
+      <a class="all-article__link" href="<?php the_permalink(); ?>">
+        <div class="all-article__post-wrap">
+          <div class="all-article__date"><?php echo get_the_date('Y.m.d'); ?></div>
+          <div class="all-article__ttl"><?php the_title(); ?></div>
+          <div class="all-article__desc"><?php the_excerpt(); ?></div>
+        </div>
+      </a>
+      <?php endwhile; ?>
+      <?php endif; ?>
+      <?php wp_reset_postdata(); ?>
+    </div>
+  </section>
+  <section class="front-sec">
+    <h2 class="front-sec__ttl">これまでに書いた記事について</h2>
+    <div class="front-sec__text">
+      <p>これまでにプログラミングや旅行についてなど、数多くの記事を書いてきました。</p>
+      <a href="<?php echo home_url("all-article"); ?>">これまでに書いた記事の一覧</a>
+    </div>
+  </section>
+  <section class="front-sec">
     <h2 class="front-sec__ttl">私について</h2>
     <div class="front-sec__text">
       <p>29歳男性です。愛知県一宮市に住んでいます。ソフトウェア開発エンジニアとして2月1日から就業開始しました。<br>これまでは、WEB制作のフリーランスエンジニアとして、WEBサイトのコーディングを行ってきました。</p>
@@ -32,13 +65,6 @@
     <div class="front-sec__text">
       <p>今年からchocoZAPに入会しました。chocoZAPはライザップが運営しているコンビニ感覚で通えるジムです。</p>
       <p>今後このブログで筋トレのメリットなどを伝えていけたらと思います。</p>
-    </div>
-  </section>
-  <section class="front-sec">
-    <h2 class="front-sec__ttl">これまでに書いた記事について</h2>
-    <div class="front-sec__text">
-      <p>これまでにプログラミングや旅行についてなど、数多くの記事を書いてきました。</p>
-      <a href="<?php echo home_url("all-article"); ?>">これまでに書いた記事の一覧</a>
     </div>
   </section>
   <section class="front-sec">
