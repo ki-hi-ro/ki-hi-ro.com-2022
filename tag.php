b5e4ad67e4535d5a7c528b1ad99c3082d5a111ca
khiro <hiroki.hiroki@icloud.com>
2023-02-11 13:37:17 +0900

    タグのメイン画像を表示させた

--- tag.php
+++ tag.php
@@ -5,6 +5,17 @@ $term_slug = $term->slug;
 $term_name = $term->name;
 $term_desc = $term->description;
 ?>
+<?php
+$tag_mv = get_template_directory_uri() . "/assets/img/tag/tag-mv-" . $term_slug . ".png";
+error_reporting(0);
+if(exif_imagetype($tag_mv)) :
+?>
+<div class="tag-mv l-container">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tag/tag-mv-<?php echo $term_slug; ?>.png" alt="<?php echo $term_name; ?>の一覧ページのメイン画像">
+  <h1><?php echo $term_name; ?></h1>
+  <div class="overlay"></div>
+</div>
+<?php endif; ?>
 <main class="l-container">
   <div class="l-pc-left">
     <!-- <section class="front-sec">
