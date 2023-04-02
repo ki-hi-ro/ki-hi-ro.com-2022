aeb7d5eca44d615f81fe79b19c931420da911f89
ki-hi-ro <hiroki.hiroki@icloud.com>
2023-04-02 14:57:55 +0900

    不要なテンプレートファイルの削除

--- template-parts/nav-blog.php
+++
@@ -1,5 +0,0 @@
-<ul>
-	<li><a href="<?php echo home_url('skill-blog'); ?>">技術ブログ</a></li>
-	<li><a href="<?php echo home_url('study-blog'); ?>">学習ブログ</a></li>
-	<li><a href="<?php echo home_url('random-blog'); ?>">雑記ブログ</a></li>
-</ul>
