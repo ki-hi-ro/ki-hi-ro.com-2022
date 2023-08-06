e827317e6f06d28305231001d1eb228e501b6f43
ki-hi-ro <hiroki.hiroki@icloud.com>
2023-08-06 15:41:50 +0900

    テンプレファイルの共通化

--- index.php
+++ index.php
@@ -1,49 +1,71 @@
 <?php get_header();?>
+<?php
+if(is_tag()) {
+  $term = get_queried_object();
+  $term_slug = $term->slug;
+  $term_name = $term->name;
+  $term_desc = $term->description;
+}
+?>
 
-<main class="l-container test">
-  <div class="l-pc-left">
-    <?php
-    $current_cat_id = get_query_var('cat');
-    $current_cat_slug = get_query_var('category_name');
-    if ($current_cat_id == "") {
-      $post_list_ttl = "これまでに書いた記事";
-    } elseif ($current_cat_id == "98") {
-      $post_list_ttl = "これまでの仕事";
-    } elseif ($current_cat_id == "60") {
-      $post_list_ttl = "技術ブログ";
-    } elseif ($current_cat_id == "69") {
-      $post_list_ttl = "学習ブログ";
-    } elseif ($current_cat_id == "67") {
-      $post_list_ttl = "雑記ブログ";
-    } elseif ($current_cat_slug == "record") {
-      $post_list_ttl = "記録";
-    } elseif ($current_cat_slug == "retrospect-and-goals") {
-      $post_list_ttl = "振り返りと目標";
-    } elseif ($current_cat_slug == "exercise-record") {
-      $post_list_ttl = "エクササイズ";
-    }
-    ?>
-    <h1 class="post-list-ttl --<?php echo $current_cat_slug; ?>"><?php echo $post_list_ttl; ?></h1>
-    <div class="new-article">
+<main class="front-container">
+  <div class="pc-left-container">
+    <section class="front-sec">
       <?php
-      $args = array(
-        'post_type' => 'post',
-        'posts_per_page' => 6,
-        'paged' => get_query_var('paged'),
-        'category' => $current_cat_id,
-        'post__not_in' => array(3874),
-      );
-      $myposts = get_posts($args);
-      ?>
-      <ul class="blog-list-grid">
-        <?php foreach ($myposts as $post): setup_postdata($post);?>
-	          <?php echo get_template_part('template-parts/blog-list-grid'); ?>
-	      <?php endforeach; wp_reset_postdata();?>
-      </ul>
-      <?php wp_pagenavi(); ?>
-    </div>
+    if(is_front_page() || is_home()) {
+      $article_list_ttl = "最近書いた";
+    } else if(is_page("all-article")) {
+      $article_list_ttl = "すべての";
+    } else if(is_tag()) {
+      $article_list_ttl = $term_name."についての";
+    } else if(is_date()) {
+      $article_list_ttl = get_query_var('year').'年'.get_query_var('monthnum').'月の';
+    } 
+    ?>
+      <h2 class="front-sec__ttl"><?php echo $article_list_ttl; ?>記事</h2>
+      <div class="front-sec__text front-sec__flex">
+        <?php
+        if(is_date()) {echo get_template_part("template-parts/blog-list-thumb-desc-date");}
+        else {echo get_template_part("template-parts/blog-list-thumb-desc");}
+        ?>
+      </div>
+      <a class="front-sec__more" href="<?php echo home_url("all-article"); ?>">これまでに書いた記事の一覧はこちら</a>
+    </section>
   </div>
-  <?php get_sidebar();?>
+  <div class="pc-right-container">
+    <section class="front-sec">
+      <h2 class="front-sec__ttl">年月アーカイブ</h2>
+      <?php echo get_template_part("template-parts/date-article-list"); ?>
+    </section>
+
+    <section class="front-sec">
+      <h2 class="front-sec__ttl">タグ</h2>
+      <div class="front-sec__text">
+        <?php
+        $args = array(
+            'orderby' => 'name',
+            'order' => 'ASC'
+        );
+        $posttags = get_tags( $args );
+
+        if ( $posttags ){
+          echo ' <ul class="front-sec__tag-list"> ';
+          $tag_count = count($posttags);
+          $i = 1;
+          foreach( $posttags as $tag ) {
+            if($i != $tag_count) {
+              echo '<li class="front-sec__item"><a class="front-sec__link" href="'. get_tag_link( $tag->term_id ) . '">' . $tag->name .  '</a></li>';
+            } else {
+              echo '<li class="front-sec__item"><a class="front-sec__link" href="'. get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a></li>';
+            }
+            $i++;
+          }
+          echo ' </ul> ';
+        }
+        ?>
+      </div>
+    </section>
+  </div>
 </main>
 
 <?php get_footer();?>
