13b67370748ca715e77055dc8de7d72badf414dd
ki-hi-ro <hiroki.hiroki@icloud.com>
2023-08-13 15:00:03 +0900

    目次デザインの変更

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
     <div class="front-container">© 2023 Shibata Hiroki. All rights reserved.</div>
 </footer>
 
-<p class="page-top --not-single-sp"><a class="page-top__link --not-single-sp" href="#">ページ<br>最上部へ</a></p>
-<p class="page-top --single-sp"><a class="page-top__link" href="#">目次へ</a></p>
+<p class="page-top --not-single-sp"><a class="page-top__link --not-single-sp" href="#">↑</a></p>
+<p class="page-top --single-sp"><a class="page-top__link" href="#">↑</a></p>
 
 <?php wp_footer(); ?>
 </body>
