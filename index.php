c272b0bfe5730feb77728cc3c458cb677d948166
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-05-03 10:50:41 +0900

    関連するタグをサイドバーに持ってきた

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ if(is_tag()) {
 ?>
 
 <main class="front-container">
-  <div class="pc-left-container">
-    <div class="search-form-wrap --sp">
-      <?php get_search_form() ; ?>
-    </div>
-    <section class="front-sec">
-    <?php
+    <div class="pc-left-container">
+        <div class="search-form-wrap --sp">
+            <?php get_search_form() ; ?>
+        </div>
+        <section class="front-sec">
+            <?php
       if(is_page("all-article")) {
       $article_list_ttl = "すべての";
     } else if(is_tag()) {
@@ -24,15 +24,55 @@ if(is_tag()) {
     }
     ?>
 
-      <?php if ( is_home() || is_front_page() ) : ?>
-        <h2 class="front-sec__ttl">最近書いた記事</h2>
-        <div class="front-sec__text front-sec__flex">
-          <?php echo get_template_part("template-parts/blog-list-thumb-desc-new"); ?>
-        </div>
-      <?php endif; ?>
+            <?php if ( is_home() || is_front_page() ) : ?>
+            <h2 class="front-sec__ttl">最近書いた記事</h2>
+            <div class="front-sec__text front-sec__flex">
+                <?php echo get_template_part("template-parts/blog-list-thumb-desc-new"); ?>
+            </div>
+            <?php endif; ?>
 
-      <?php if ( !(is_home() || is_front_page()) ) : ?>
-        <?php if( !(is_date() || is_page("all-article")) ) : ?>
+            <?php if ( !(is_home() || is_front_page()) ) : ?>
+            <h2 class="front-sec__ttl <?php if(is_tag()): ?>tag_article_ttl<?php endif; ?>"><?php echo $article_list_ttl; ?>記事</h2>
+            <div class="front-sec__text front-sec__flex">
+                <?php
+          if(is_date()) {
+            echo get_template_part("template-parts/blog-list-thumb-desc-date");
+          } else {
+            echo get_template_part("template-parts/blog-list-thumb-desc");
+          }
+          ?>
+            </div>
+            <?php endif; ?>
+
+            <!-- <?php if ( is_home() || is_front_page() ) : ?>
+        <h2 class="front-sec__ttl">今の自分にとって重要な記事</h2>
+        <div class="front-sec__text front-sec__flex">
+          <?php echo get_template_part("template-parts/blog-list-thumb-desc_important"); ?>
+        </div>
+      <?php endif; ?> -->
+
+            <?php if ( is_home() || is_front_page() ) : ?>
+            <?php if(!is_page("all-article")) : ?>
+            <h2 class="front-sec__ttl rand">ランダムに表示される記事</h2>
+            <div class="front-sec__text front-sec__flex">
+                <?php echo get_template_part("template-parts/blog-list-thumb-desc_rand"); ?>
+            </div>
+            <?php endif; ?>
+            <?php endif; ?>
+
+            <?php if ( (is_home() || is_front_page()) ) : ?>
+            <a class="front-sec__more" href="<?php echo home_url("all-article"); ?>">すべての記事はこちら</a>
+            <?php else :  ?>
+            <a class="front-sec__more" href="<?php echo home_url(); ?>">トップページはこちら</a>
+            <?php endif; ?>
+        </section>
+    </div>
+    <div class="pc-right-container">
+        <div class="search-form-wrap --pc">
+            <?php get_search_form() ; ?>
+        </div>
+        <section class="front-sec">
+            <?php if( is_tag() ) : ?>
             <?php 
             function tagChild($parentId, $childTags) {
               if(is_tag()) {
@@ -67,57 +107,17 @@ if(is_tag()) {
             tagChild(81,array(255,238,236,237,288,273));
             tagChild(210,array(289));
             ?>
-        <?php endif; ?>
-        <h2 class="front-sec__ttl <?php if(is_tag()): ?>tag_article_ttl<?php endif; ?>"><?php echo $article_list_ttl; ?>記事</h2>
-        <div class="front-sec__text front-sec__flex">
-          <?php
-          if(is_date()) {
-            echo get_template_part("template-parts/blog-list-thumb-desc-date");
-          } else {
-            echo get_template_part("template-parts/blog-list-thumb-desc");
-          }
-          ?>
-        </div>
-      <?php endif; ?>
-
-      <!-- <?php if ( is_home() || is_front_page() ) : ?>
-        <h2 class="front-sec__ttl">今の自分にとって重要な記事</h2>
-        <div class="front-sec__text front-sec__flex">
-          <?php echo get_template_part("template-parts/blog-list-thumb-desc_important"); ?>
-        </div>
-      <?php endif; ?> -->
-
-      <?php if ( is_home() || is_front_page() ) : ?>
-        <?php if(!is_page("all-article")) : ?>
-          <h2 class="front-sec__ttl rand">ランダムに表示される記事</h2>
-          <div class="front-sec__text front-sec__flex">
-            <?php echo get_template_part("template-parts/blog-list-thumb-desc_rand"); ?>
-          </div>
-        <?php endif; ?>
-      <?php endif; ?> 
-      
-      <?php if ( (is_home() || is_front_page()) ) : ?>
-        <a class="front-sec__more" href="<?php echo home_url("all-article"); ?>">すべての記事はこちら</a>
-      <?php else :  ?>
-        <a class="front-sec__more" href="<?php echo home_url(); ?>">トップページはこちら</a>
-      <?php endif; ?>
-    </section>
-  </div>
-  <div class="pc-right-container">
-    <div class="search-form-wrap --pc">
-      <?php get_search_form() ; ?>
+            <?php endif; ?>
+            <section class="front-sec">
+                <h2 class="front-sec__ttl">年月アーカイブ</h2>
+                <?php echo get_template_part("template-parts/date-article-list"); ?>
+            </section>
+            <h2 class="front-sec__ttl">タグ</h2>
+            <div class="front-sec__text">
+                <?php wp_tag_cloud('format=list&smallest=16&largest=16&unit=px&number=200&exclude=116'); ?>
+            </div>
+        </section>
     </div>
-    <section class="front-sec">
-      <section class="front-sec">
-        <h2 class="front-sec__ttl">年月アーカイブ</h2>
-        <?php echo get_template_part("template-parts/date-article-list"); ?>
-      </section>
-      <h2 class="front-sec__ttl">タグ</h2>
-      <div class="front-sec__text">
-        <?php wp_tag_cloud('format=list&smallest=16&largest=16&unit=px&number=200&exclude=116'); ?>
-      </div>
-    </section>
-  </div>
 </main>
 
 <?php get_footer();?>
