979fc818977e758d000c043a014cbdcc9aa88a95
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-06-28 17:42:53 +0900

    投稿IDを表示

--- single.php
+++ single.php
@@ -55,6 +55,9 @@ if($posttags) {
                 </li>
                 <?php endif; ?>
             </ul>
+            <div class="post-id">
+                投稿ID : <?php the_ID(); ?>
+            </div>
             <?php echo get_template_part('template-parts/single-bread'); ?>
             <?php endif; ?>
             <?php endwhile; endif; ?>
