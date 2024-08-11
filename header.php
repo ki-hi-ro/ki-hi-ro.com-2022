82f82b5e430cb4cedb1e001d1cc77f10c7df381a
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-08-11 13:52:35 +0900

    リファクタリング

--- header.php
+++ header.php
@@ -2,60 +2,59 @@
 <html lang="ja">
 
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MLVT1B3MQP"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-MLVT1B3MQP"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
 
-    gtag('config', 'G-MLVT1B3MQP');
-    </script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php if (is_archive() || is_search() || is_tag() || is_paged()) : ?>
-    <meta name="robots" content="noindex,follow">
-    <?php endif; ?>
+  gtag('config', 'G-MLVT1B3MQP');
+  </script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php if (is_archive() || is_search() || is_tag() || is_paged()) : ?>
+  <meta name="robots" content="noindex,follow">
+  <?php endif; ?>
 
-    <title>
-        <?php
-		global $page, $paged;
-		if (is_single() || is_page() || is_archive() || is_search()) {
-			wp_title('|', true, 'right');
-		} elseif (is_404()) {
-			echo '404 |';
-		}
-		bloginfo('name');
-		if ($paged >= 2 || $page >= 2) {
-			echo '-' . sprintf('%sページ', max($paged, $page));
-		}
-		?>
-    </title>
-    <meta name=”description” content=”2019年末、TECH::EXPERTでプログラミングを経験後に、WEB制作のフリーランスとして活動を続けてきました。このブログでは、「これまでの仕事」「技術」「学習」「雑記」の4つのカテゴリーで発信していきます。“>
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
+  <title>
+    <?php
+	global $page, $paged;
+	if (is_single() || is_page() || is_archive() || is_search()) {
+      wp_title('|', true, 'right');
+	} elseif (is_404()) {
+	  echo '404 |';
+	}
+	  bloginfo('name');
+	if ($paged >= 2 || $page >= 2) {
+	  echo '-' . sprintf('%sページ', max($paged, $page));
+	}
+	?>
+  </title>
+  <meta name=”description” content=”WEBエンジニアを目指して活動中の31歳です。自分の考えについてのブログなども書いています。“>
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
 
-    <!-- schema.org -->
-    <meta itemprop="name" content="<?php the_title(''); ?>">
-    <meta itemprop="description" content="<?php the_permalink() ?>">
+  <!-- schema.org -->
+  <meta itemprop="name" content="<?php the_title(''); ?>">
+  <meta itemprop="description" content="<?php the_permalink() ?>">
 
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <?php wp_head(); ?>
+  <!-- <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" /> -->
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class('kihiro-body front-page'); ?>>
-
-    <header id="header" class="header">
-        <div class="header__container">
-            <div class="header__contents">
-                <div class="header__logo">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/site-name-logo-4.svg" alt="ロゴ">
-                    <a class="header__sitename-link" href="<?php echo home_url(); ?>">
-                        <span class="header__sitename-text">ki-hi-ro.com</span>
-                    </a>
-                </div>
-            </div>
+  <header>
+    <div class="header__container">
+      <div class="header__contents">
+        <div class="header__logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/site-name-logo-4.svg" alt="ロゴ">
+          <a class="header__sitename-link" href="<?php echo home_url(); ?>">
+            <span class="header__sitename-text">ki-hi-ro.com</span>
+          </a>
         </div>
-    </header>
+      </div>
+    </div>
+  </header>
--- index.php
+++ index.php
@@ -1,115 +1,86 @@
-<?php get_header();?>
-<?php
-if(is_tag()) {
-  $term = get_queried_object();
-  $term_slug = $term->slug;
-  $term_name = $term->name;
-  $term_desc = $term->description;
-  $term_id = $term->term_id;
-}
+<?php 
+get_header();
+if(is_tag()) { $term = get_queried_object(); } 
 ?>
-
 <main class="front-container">
   <div class="pc-left-container">
     <div class="search-form-wrap --sp">
       <?php get_search_form() ; ?>
     </div>
     <section class="front-sec">
-      <?php if (is_search()) : ?> 
+      <?php if (is_search()) : ?>
       <h2 class="front-sec__ttl">"<?php echo get_search_query(); ?>" が本文中に含まれている記事</h2>
       <div class="front-sec__text front-sec__flex">
+        <?php if (have_posts()): while(have_posts()) : the_post(); ?>
+        <div class="all-article__link front-sec__flex-item">
+          <?php echo get_template_part("template-parts/blog-list"); ?>
+        </div>
         <?php
-        if (have_posts()):
-          while(have_posts()) : the_post();
-        ?>
-          <div class="all-article__link front-sec__flex-item">
-            <?php echo get_template_part("template-parts/blog-list"); ?>
-          </div>
-          <?php
           endwhile;
         else:
           ?>
-          <p>該当する記事はありませんでした。</p>
-          <?php endif; ?>
-          <?php endif; ?>
-          <?php
+        <p>該当する記事はありませんでした。</p>
+        <?php endif; ?>
+        <?php endif; ?>
+        <?php
           if(is_page("all-article")) {
             $article_list_ttl = "すべての";
           } else if(is_tag()) {
-            $article_list_ttl = $term_name."についての";
+            $article_list_ttl = $term->name."についての";
           } else if(is_date()) {
             $article_list_ttl = get_query_var('year').'年'.get_query_var('monthnum').'月の';
           }
           ?>
-
-          <?php if ( is_home() || is_front_page() ) : ?>
-            <h2 class="front-sec__ttl">最近書いた記事</h2>
-            <div class="front-sec__text front-sec__flex">
-                <?php echo get_template_part("template-parts/blog-list-thumb-desc-new"); ?>
-            </div>
-          <?php endif; ?>
-
-          <?php if ( !(is_home() || is_front_page() || is_search() ) ) : ?>
-            <h2 class="front-sec__ttl <?php if(is_tag()): ?>tag_article_ttl<?php endif; ?>"><?php echo $article_list_ttl; ?>記事<?php if(is_tag()): ?>（<?php echo $wp_query->post_count; ?>）<?php endif; ?></h2>
-            <div class="front-sec__text front-sec__flex">
-              <?php
+        <?php if ( is_home() || is_front_page() ) : ?>
+        <h2 class="front-sec__ttl">最近書いた記事</h2>
+        <div class="front-sec__text front-sec__flex">
+          <?php echo get_template_part("template-parts/blog-list-thumb-desc-new"); ?>
+        </div>
+        <?php endif; ?>
+        <?php if ( !(is_home() || is_front_page() || is_search() ) ) : ?>
+        <h2 class="front-sec__ttl <?php if(is_tag()): ?>tag_article_ttl<?php endif; ?>"><?php echo $article_list_ttl; ?>記事<?php if(is_tag()): ?>（<?php echo $term->count; ?>）<?php endif; ?></h2>
+        <div class="front-sec__text front-sec__flex">
+          <?php
               if(is_date()) {
                 echo get_template_part("template-parts/blog-list-thumb-desc-date");
               } else {
                 echo get_template_part("template-parts/blog-list-thumb-desc");
               }
               ?>
-            </div>
-          <?php endif; ?>
-
-          <?php if ( (is_home() || is_front_page()) ) : ?>
-            <a class="front-sec__more" href="<?php echo home_url("all-article"); ?>">すべての記事はこちら</a>
-          <?php else : ?>
-            <a class="front-sec__more" href="<?php echo home_url(); ?>">トップページはこちら</a>
-          <?php endif; ?>
-        </section>
-    </div>
-    <div class="pc-right-container">
-        <div class="search-form-wrap --pc">
-            <?php get_search_form() ; ?>
         </div>
-        <section class="front-sec">
-
-            <section class="front-sec">
-              <h2 class="front-sec__ttl">タグ</h2>
-              <div class="front-sec__text">
-                <?php 
-                $tag_groups = array(
-                  'プログラミング' => array(148,292,334,322,52,74,318,290,289,287,277,173,204,119,140,117,193,84,87,221,337),
-                  '青春18きっぷ' => array(188,233,234,286),
-                );
-                
-                $include_tag_ids = [];
-                // $exclude_tag_ids = [116];
-                $exclude_tag_id = '';
-                foreach ($tag_groups as $tag_group_key => $tag_group_value) {
-                  if(is_tag($tag_group_value)) {
-                    $include_tag_ids = $tag_group_value;
-                    $exclude_tag_id = $term_id;
-                  }
-                }
-                
-                $tag_cloud_settings = [
-                  'number' => 0, 'smallest' => 16, 'largest' => 16, 'format' => 'list', 'unit' => 'px',
-                  'exclude' => 116,
-                  // 'include' => $include_tag_ids
-                ];
-                wp_tag_cloud($tag_cloud_settings); 
-                ?>
-              </div>
-            </section>
-
-            <section class="front-sec">
-                <h2 class="front-sec__ttl">年月アーカイブ</h2>
-                <?php echo get_template_part("template-parts/date-article-list"); ?>
-            </section>
-        </section>
+        <?php endif; ?>
+        <?php if ( (is_home() || is_front_page()) ) : ?>
+        <a class="front-sec__more" href="<?php echo home_url("all-article"); ?>">すべての記事はこちら</a>
+        <?php else : ?>
+        <a class="front-sec__more" href="<?php echo home_url(); ?>">トップページはこちら</a>
+        <?php endif; ?>
+    </section>
+  </div>
+  <div class="pc-right-container">
+    <div class="search-form-wrap --pc">
+      <?php get_search_form() ; ?>
     </div>
+    <section class="front-sec">
+      <section class="front-sec">
+        <h2 class="front-sec__ttl">タグ</h2>
+        <div class="front-sec__text">
+          <?php 
+          $tag_groups = array(
+            'プログラミング' => array(148,292,334,322,52,74,318,290,289,287,277,173,204,119,140,117,193,84,87,221,337),
+            '青春18きっぷ' => array(188,233,234,286),
+          );
+          $tag_cloud_settings = [
+            'number' => 0, 'smallest' => 16, 'largest' => 16, 'format' => 'list', 'unit' => 'px', 'exclude' => 116,
+          ];
+          wp_tag_cloud($tag_cloud_settings); 
+          ?>
+        </div>
+      </section>
+      <section class="front-sec">
+        <h2 class="front-sec__ttl">年月アーカイブ</h2>
+        <?php echo get_template_part("template-parts/date-article-list"); ?>
+      </section>
+    </section>
+  </div>
 </main>
-
 <?php get_footer();?>
