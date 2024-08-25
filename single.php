625244c48040adb01c60c90ed7f748e1d89d4263
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-08-25 09:06:26 +0900

    同じタグの記事のみを前後のリンクに表示させた

--- single.php
+++ single.php
@@ -13,57 +13,57 @@ if($posttags) {
 }
 ?>
 <main class="l-container">
-    <div class="l-pc-left --single">
-        <article class="post">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php echo get_template_part('template-parts/single-bread'); ?>
-            <div class="all-article__date"><?php echo get_the_date('Y.m.d'); ?>（更新日: <?php echo get_the_modified_date('Y.m.d'); ?>）</div>
-            <h1 class="post__ttl"><?php the_title(); ?></h1>
-            <?php the_post_thumbnail( array( 366, 244 ), ['class' => 'post__thumb']  );?>
-            <?php if(!in_category('record')): ?>
-            <h2 id="first-ttl">はじめに</h2>
-            <?php endif; ?>
-            <div class="post__content">
-                <?php
+  <div class="l-pc-left --single">
+    <article class="post">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php echo get_template_part('template-parts/single-bread'); ?>
+      <div class="all-article__date"><?php echo get_the_date('Y.m.d'); ?>（更新日: <?php echo get_the_modified_date('Y.m.d'); ?>）</div>
+      <h1 class="post__ttl"><?php the_title(); ?></h1>
+      <?php the_post_thumbnail( array( 366, 244 ), ['class' => 'post__thumb']  );?>
+      <?php if(!in_category('record')): ?>
+      <h2 id="first-ttl">はじめに</h2>
+      <?php endif; ?>
+      <div class="post__content">
+        <?php
                 if(in_category('record')) :
                     get_template_part('template-parts/record');
                 else :
                     the_content();
                 endif;
                 ?>
-            </div>
+      </div>
 
-            <?php
-            $prevpost = get_adjacent_post(false, '', true);
-            $nextpost = get_adjacent_post(false, '', false);
-            if( $prevpost or $nextpost) :
-            ?>
-            <?php comments_template(); ?>
-            <ul class="nav-links">
-                <?php if( $prevpost ) : ?>
-                <li class="nav-links__nav --pre">
-                    <a class="nav-links__link" href="<?php echo get_permalink($prevpost->ID); ?>">
-                        ← <?php echo get_the_title( $prevpost->ID ); ?>
-                    </a>
-                </li>
-                <?php endif; ?>
-                <?php if( $nextpost ) : ?>
-                <li class="nav-links__nav --next">
-                    <a class="nav-links__link" href="<?php echo get_permalink($nextpost->ID); ?>">
-                        <?php echo get_the_title( $nextpost->ID ); ?> →
-                    </a>
-                </li>
-                <?php endif; ?>
-            </ul>
-            <div class="post-id">
-                投稿ID : <?php the_ID(); ?>
-            </div>
-            <?php echo get_template_part('template-parts/single-bread'); ?>
-            <?php endif; ?>
-            <?php endwhile; endif; ?>
+      <?php
+      $prevpost = get_adjacent_post(true, '', true, 'post_tag');
+      $nextpost = get_adjacent_post(true, '', false, 'post_tag');
+      if( $prevpost or $nextpost) :
+      ?>
+      <?php comments_template(); ?>
+      <ul class="nav-links">
+        <?php if( $prevpost ) : ?>
+        <li class="nav-links__nav --pre">
+          <a class="nav-links__link" href="<?php echo get_permalink($prevpost->ID); ?>">
+            ← <?php echo get_the_title( $prevpost->ID ); ?>
+          </a>
+        </li>
+        <?php endif; ?>
+        <?php if( $nextpost ) : ?>
+        <li class="nav-links__nav --next">
+          <a class="nav-links__link" href="<?php echo get_permalink($nextpost->ID); ?>">
+            <?php echo get_the_title( $nextpost->ID ); ?> →
+          </a>
+        </li>
+        <?php endif; ?>
+      </ul>
+      <div class="post-id">
+        投稿ID : <?php the_ID(); ?>
+      </div>
+      <?php echo get_template_part('template-parts/single-bread'); ?>
+      <?php endif; ?>
+      <?php endwhile; endif; ?>
 
-        </article>
-    </div>
-    <?php get_sidebar(); ?>
+    </article>
+  </div>
+  <?php get_sidebar(); ?>
 </main>
 <?php get_footer(); ?>
