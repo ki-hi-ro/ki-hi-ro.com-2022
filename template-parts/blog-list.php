cf41f6c9e3eb531fe57877602b3197cc3bb5ec3f
ki-hi-ro <hiroki.hiroki@icloud.com>
2024-11-17 11:33:18 +0900

    全ての記事でタイトルのみ表示

--- template-parts/blog-list.php
+++ template-parts/blog-list.php
@@ -1,6 +1,7 @@
 <div class="all-article__post-wrap">
     <div class="all-article__main">
       <div class="all-article__text-wrap">
+        <?php if (!is_page('all-article')) : ?>
         <div class="all-article__tag-date">
           <?php
           $flag = get_the_tags(); foreach ( (array)$flag as $tag ) {} if($tag) :
@@ -24,18 +25,21 @@
             <div class="all-article__date"><?php echo get_the_date('Y.m.d'); ?>（更新日: <?php echo get_the_modified_date('Y.m.d'); ?>）</div>
           </div>
         </div>
-        <div class="all-article__text">
-          <a class="all-article__href" href="<?php the_permalink(); ?>">
+        <?php endif; ?>
+        <a class="all-article__href" href="<?php the_permalink(); ?>" target="blank">
+          <div class="all-article__text">
             <div class="all-article__ttl"><?php the_title(); ?></div>
-          </a>
-          <div class="all-article__desc">
-            <p class="all-article__desc-pc"><?php echo custom_excerpt_pc(); ?></p>
-            <p class="all-article__desc-sp"><?php echo custom_excerpt_sp(); ?></p>
-            <?php if (has_post_thumbnail()) : the_post_thumbnail('',array( 'class' => 'front-sec__flex-item-thumb' )); else : ?>
-              <img class="front-sec__flex-item-thumb" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image">
-            <?php endif; ?>
+              <?php if (!is_page('all-article')) : ?>
+              <div class="all-article__desc">
+                <p class="all-article__desc-pc"><?php echo custom_excerpt_pc(); ?></p>
+                <p class="all-article__desc-sp"><?php echo custom_excerpt_sp(); ?></p>
+                <?php if (has_post_thumbnail()) : the_post_thumbnail('',array( 'class' => 'front-sec__flex-item-thumb' )); else : ?>
+                  <img class="front-sec__flex-item-thumb" src="<?php echo get_template_directory_uri(); ?>/assets/img/blog/no-image.png" alt="no-image">
+                <?php endif; ?>
+              </div>
+              <?php endif; ?>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
-    </div>
 </div>
