b4aac35c86befa912ca59e496775f78c034b70d1
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-03-09 00:53:13 +0900

    フッターの文言を変更

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php wp_reset_query(); ?>
 
 <footer class="footer">
-    <div class="front-container">© 2024 Shibata Hiroki. All rights reserved.</div>
+    <div class="front-container">WEBエンジニア 柴田浩貴のブログ</div>
 </footer>
 
 <p class="page-top --not-single-sp"><a class="page-top__link --not-single-sp" href="#">↑</a></p>
