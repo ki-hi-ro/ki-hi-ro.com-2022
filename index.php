0505aeb47c1ce7586942302ebf0f5cb2f37c4a5d
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-03-09 00:06:23 +0900

    サイドバーに検索バーとタグのみを表示

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ if(is_tag()) {
     $term = get_queried_object(); 
 } 
 ?>
+
 <?php if (is_date() && get_query_var('year') == 2025 && get_query_var('monthnum') == 3) : ?>
   <div class="callender-img front-container">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/202503.jpeg" alt="2025年3月のカレンダー画像">
@@ -19,27 +20,47 @@ if(is_tag()) {
 
     <section class="front-sec">
       <?php if (is_search()) : ?>
-      <h2 class="front-sec__ttl">"<?php echo get_search_query(); ?>" が本文中に含まれている記事</h2>
-      <div class="front-sec__text front-sec__flex">
-        <?php if (have_posts()): while(have_posts()) : the_post(); ?>
-        <div class="all-article__link front-sec__flex-item">
-          <?php get_template_part("template-parts/blog-list"); ?>
+        <h2 class="front-sec__ttl">"<?php echo get_search_query(); ?>" が本文中に含まれている記事</h2>
+        <div class="front-sec__text front-sec__flex">
+          <?php if (have_posts()): while(have_posts()) : the_post(); ?>
+            <div class="all-article__link front-sec__flex-item">
+              <?php get_template_part("template-parts/blog-list"); ?>
+            </div>
+          <?php endwhile; else: ?>
+            <p>該当する記事はありませんでした。</p>
+          <?php endif; ?>
         </div>
-        <?php endwhile; else: ?>
-        <p>該当する記事はありませんでした。</p>
-        <?php endif; ?>
-      </div>
       <?php endif; ?>
 
-      <?php if ( is_home() || is_front_page() ) : ?>
-        <?php get_template_part("template-parts/front-posts"); ?>
-      <?php endif; ?>
+      <?php 
+      $is_front = is_home() || is_front_page();
+      if ($is_front) : 
+        get_template_part("template-parts/front-posts");
+      endif;
+      ?>
 
-      <?php if ( !(is_home() || is_front_page() || is_search() ) ) : ?>
+      <?php 
+      // デフォルト値を設定
+      $article_list_ttl = "記事";
+
+      if (is_page("all-article")) {
+          $article_list_ttl = "すべての";
+      } elseif (is_tag() && $term) {
+          $article_list_ttl = esc_html($term->name) . "についての";
+      } elseif (is_date()) {
+          if (get_query_var('monthnum')) {
+              $article_list_ttl = get_query_var('year') . '年' . get_query_var('monthnum') . '月の';
+          } else {
+              $article_list_ttl = get_query_var('year') . '年の';
+          }
+      }
+      ?>
+
+      <?php if (!$is_front && !is_search()) : ?>
         <h2 class="front-sec__ttl"><?php echo $article_list_ttl; ?>記事</h2>
         <div class="front-sec__text front-sec__flex">
           <?php
-          if(is_date()) {
+          if (is_date()) {
               get_template_part("template-parts/blog-list-thumb-desc-date");
           } else {
               get_template_part("template-parts/blog-list-thumb-desc");
@@ -47,10 +68,10 @@ if(is_tag()) {
           ?>
         </div>
       <?php endif; ?>
-
     </section>
   </div>
-  <?php get_sidebar('front') ?>
+
+  <?php get_sidebar('front'); ?>
 </main>
 
 <?php get_footer(); ?>
--- sidebar-front.php
+++ sidebar-front.php
@@ -1,10 +1,10 @@
 <div class="pc-right-container">
     <?php get_template_part('template-parts/search-form'); ?>
     <section class="front-sec">
-        <?php get_template_part('template-parts/archive-accordion'); ?>
+        <?php //get_template_part('template-parts/archive-accordion'); ?>
         <?php get_template_part('template-parts/tags-list'); ?>
     </section>
 </div>
 
-<?php get_template_part('template-parts/accordion-script'); ?>
-<?php get_template_part('template-parts/accordion-style'); ?>
+<?php //get_template_part('template-parts/accordion-script'); ?>
+<?php //get_template_part('template-parts/accordion-style'); ?>
