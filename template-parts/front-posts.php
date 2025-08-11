c93ffb9c092415ca9eb6489c72dcb1a9d0fc6c7c
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-08-11 22:57:31 +0900

    タグ一覧をスマホで表示させた

--- template-parts/front-posts.php
+++ template-parts/front-posts.php
@@ -2,9 +2,16 @@
 // 最近書いた記事
 get_template_part('template-parts/new-posts');
 
-// 最近更新した記事
-// get_template_part('template-parts/recent-updated');
-
-// ランダム記事
-// get_template_part('template-parts/random-posts');
+// ▼タグ一覧追加
+$tags = get_tags();
+if ($tags) :
+    echo '<div class="tag-list">';
+    echo '<h2 class="front-sec__ttl --sp-center">タグ一覧</h2>';
+    echo '<ul class="front-sec__text front-sec__flex">';
+    foreach ($tags as $tag) {
+        echo '<li><a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a></li>';
+    }
+    echo '</ul>';
+    echo '</div>';
+endif;
 ?>
