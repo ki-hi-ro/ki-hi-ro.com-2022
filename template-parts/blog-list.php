d130237805a83bf529a4e1042c0b8e6b61ff1bc7
ki-hi-ro <hiroki.hiroki@icloud.com>
2025-10-27 00:22:26 +0900

    記事リストのサムネを削除した

--- template-parts/blog-list.php
+++ template-parts/blog-list.php
@@ -6,11 +6,6 @@
       <?php echo esc_html(get_the_title()); ?>
     </div>
     <div class="pc-flex">
-      <?php if (has_post_thumbnail()): ?>
-        <div class="all-article__thumb">
-          <?php the_post_thumbnail('large'); ?>
-        </div>
-      <?php endif; ?>
       <div class="all-article__desc">
           <?php echo esc_html(get_the_excerpt()); ?>
       </div>
