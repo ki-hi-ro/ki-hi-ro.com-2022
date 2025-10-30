96e8789e0cd8964f52559d562105553cbd5eb664
柴田浩貴 <hiroki@shibatahiroshitakanoMacBook-Air.local>
2025-10-30 10:22:00 +0900

    admin barの表示について修正

--- functions.php
+++ functions.php
@@ -215,10 +215,23 @@ add_action('wp_head','my_meta_ogp');
       }
   
       // -------------------------
-      // トップページ：新規投稿リンク
+      // トップページ：管理画面ロゴ＆新規投稿リンク
       // -------------------------
       if (is_front_page() || is_home()) {
           if (current_user_can('edit_posts')) {
+
+              // 管理画面トップへ飛ぶロゴアイコンを追加
+              $wp_admin_bar->add_node([
+                  'id'    => 'admin-logo',
+                  'title' => '<span class="ab-icon"></span><span class="ab-label">管理画面</span>',
+                  'href'  => admin_url(),
+                  'meta'  => [
+                      'title' => '管理画面トップへ移動',
+                      'class' => 'admin-logo-link',
+                  ],
+              ]);
+
+              // 新規投稿リンク
               $wp_admin_bar->add_node([
                   'id'    => 'new-post',
                   'title' => '新規投稿',
