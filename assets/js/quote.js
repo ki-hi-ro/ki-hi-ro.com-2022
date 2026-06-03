3e11d8d28a1e4b15865eb2998fd6aa866a00522a
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-06-03 17:37:29 +0900

    重複エラーに対処した

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,15 +1,13 @@
-const ajaxUrl = "<?php echo admin_url('admin-ajax.php'); ?>";
+function updateQuote() {
+    fetch("<?php echo admin_url('admin-ajax.php'); ?>?action=get_random_quote")
+        .then(response => response.json())
+        .then(data => {
+            if (!data) return;
 
-  function updateQuote() {
-      fetch(ajaxUrl + "?action=get_random_quote")
-          .then(response => response.json())
-          .then(data => {
-              if (!data) return;
+            document.getElementById("quote-box").href = data.url;
+            document.getElementById("quote-text").textContent = data.text;
+        })
+        .catch(error => console.error(error));
+}
 
-              document.getElementById("quote-box").href = data.url;
-              document.getElementById("quote-text").textContent = data.text;
-          })
-          .catch(error => console.error(error));
-  }
-
-  setInterval(updateQuote, 5000);
+setInterval(updateQuote, 5000);
