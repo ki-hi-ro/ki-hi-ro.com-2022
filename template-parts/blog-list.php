ae4f2be0dead0892d53d3f81b62e83d355bdd583
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-07-02 11:44:43 +0900

    調整

--- template-parts/blog-list.php
+++ template-parts/blog-list.php
@@ -3,6 +3,8 @@ $post_tags        = get_the_tags();
 $visible_tags     = $post_tags ? array_slice($post_tags, 0, 4) : array();
 $hidden_tag_count = $post_tags ? max(0, count($post_tags) - count($visible_tags)) : 0;
 $tag_slugs        = $post_tags ? wp_list_pluck($post_tags, 'slug') : array();
+$default_thumbnail_url = get_theme_file_uri('/assets/img/logbook-default-thumbnail.jpg');
+$thumbnail_error_fallback = "this.onerror=null;this.removeAttribute('srcset');this.removeAttribute('sizes');this.src=this.dataset.defaultSrc;this.classList.add('all-article__media-img--default');";
 ?>
 
 <a class="all-article__post-wrap" href="<?php the_permalink(); ?>" data-logbook-item data-logbook-tags="<?php echo esc_attr(implode(' ', $tag_slugs)); ?>" data-logbook-date="<?php echo esc_attr(get_the_date('Y-m-d')); ?>">
@@ -11,6 +13,14 @@ $tag_slugs        = $post_tags ? wp_list_pluck($post_tags, 'slug') : array();
       <?php echo get_the_date('Y.m.d'); ?>
     </time>
   </div>
+
+  <figure class="all-article__media" aria-hidden="true">
+    <?php if (has_post_thumbnail()) : ?>
+      <?php the_post_thumbnail('medium_large', array('class' => 'all-article__media-img', 'alt' => '', 'loading' => 'lazy', 'decoding' => 'async', 'data-default-src' => $default_thumbnail_url, 'onerror' => $thumbnail_error_fallback)); ?>
+    <?php else : ?>
+      <img class="all-article__media-img all-article__media-img--default" src="<?php echo esc_url($default_thumbnail_url); ?>" alt="" width="900" height="900" loading="lazy" decoding="async">
+    <?php endif; ?>
+  </figure>
 
   <div class="all-article__content">
     <div class="all-article__ttl">
