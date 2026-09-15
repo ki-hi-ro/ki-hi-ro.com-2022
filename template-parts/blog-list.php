9ed9a4cff8567c01460bf01229ed6da120a1a968
ki-hi-ro <hiroki.hiroki@icloud.com>
2026-09-15 21:32:12 +0900

    記事一覧の日付を削除

--- template-parts/blog-list.php
+++ template-parts/blog-list.php
@@ -1,8 +1,8 @@
 <article class="journal-entry">
   <a class="journal-entry__link" href="<?php the_permalink(); ?>">
-    <time class="journal-entry__date" datetime="<?php echo esc_attr(get_the_date('Y-m-d')); ?>">
+    <!-- <time class="journal-entry__date" datetime="<?php echo esc_attr(get_the_date('Y-m-d')); ?>">
       <?php echo esc_html(get_the_date('Y.m.d')); ?>
-    </time>
+    </time> -->
     <span class="journal-entry__body">
       <span class="journal-entry__title"><?php echo esc_html(get_the_title()); ?></span>
     </span>
